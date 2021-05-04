ae2cf5d4e09847b6b83cb909607562b927d634fa
a-wilkes <a.sjwilkes@gmail.com>
2021-05-04 11:51:12 +0200

    feat(sidebar): add sidebar component

--- src/app/modules/backbone/backbone.module.ts
+++ src/app/modules/backbone/backbone.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { BackboneRoutingModule } from './backbone-routing.module';
 
 import { componentDeclarations, pageDeclarations } from '../backbone/backbone.common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
       ...pageDeclarations,
-      ...componentDeclarations
+      ...componentDeclarations,
+      SidebarComponent
   ],
   imports: [
     CommonModule,
