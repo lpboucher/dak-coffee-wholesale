db1be12f58b9edbf5333ad3a11ac35089c9fd3a4
a-wilkes <a.sjwilkes@gmail.com>
2021-05-04 10:19:00 +0200

    refactor(backbone): remove excess spacing

--- src/app/modules/backbone/backbone.module.ts
+++ src/app/modules/backbone/backbone.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { BackbonePageComponent } from './pages/backbone/backbone.page';
 import { BackboneComponent } from './components/backbone/backbone.component';
-
-
 
 @NgModule({
   declarations: [BackbonePageComponent, BackboneComponent],
