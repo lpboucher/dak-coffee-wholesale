1bc990e8c6daa8aba14e4ee1a34b200f4df300c9
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2021-05-08 16:55:46 +0200

    change(product-list ts): use interfaces

--- src/app/modules/products/components/product-list/product-list.component.ts
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from "rxjs";
 
-import { Product } from "@modules/products/product.interface";
+import { Product } from "@app/shared/models/classes/product.interface";
+import { Coffee } from "@app/shared/models/classes/coffee.interface";
 
 @Component({
   selector: 'app-product-list',
@@ -9,87 +10,109 @@ import { Product } from "@modules/products/product.interface";
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-    products$: Observable<Product[]> = new Observable();
-    products: Product[] = [
+    products$: Observable<(Coffee | Product)[]> = new Observable();
+    products: (Coffee | Product)[] = [
         {
             id: 0,
             name: "The Alchemist",
             origin: "Rawanda",
             price: "$15.00",
-            description: "Experimental Natural, Bourbon",
-            tastingNotes: "Mango, Pineapple, Kiwi, Honey, Dark Chocolate"
+            process: "Experimental Natural",
+            varietal: "Bourbon",
+            description: "",
+            tastingNotes: "Mango, Pineapple, Kiwi, Honey, Dark Chocolate",
+            type: "coffee"
         },
         {
             id: 1,
             name: "La Terraza",
             origin: "Colombia",
             price: "$24.00",
+            process: "Honey",
+            varietal: "Geisha",
             description: "Honey, Geisha",
-            tastingNotes: "White Flower, Pomegranate, Honeycomb, Raspberry"
+            tastingNotes: "White Flower, Pomegranate, Honeycomb, Raspberry",
+            type: "coffee"
         },
         {
             id: 2,
             name: "La Dalia",
             origin: "Salvador",
             price: "$15.00",
+            process: "48hrs Anaerobic",
+            varietal: "Pacamara",
             description: "48hrs Anaerobic, Pacamara",
-            tastingNotes: "Green Apple, Red Currant, Strawberry"
+            tastingNotes: "Green Apple, Red Currant, Strawberry",
+            type: "coffee"
         },
         {
             id: 3,
             name: "El Diviso",
             origin: "Colombia",
             price: "$16.00",
+            process: "Washed",
+            varietal: "Pink Bourbon",
             description: "Washed, Pink Bourbon",
-            tastingNotes: "Grapefruit, Orange, Peach, Sugar"
+            tastingNotes: "Grapefruit, Orange, Peach, Sugar",
+            type: "coffee"
         },
         {
             id: 4,
             name: "Rusatira",
             origin: "Rwanda",
             price: "$15.90",
+            process: "Wet Natural Anaerobic",
+            varietal: "Red Bourbon",
             description: "Wet Natural Anaerobic, Red Bourbon",
-            tastingNotes: "Red Grapes, Peach, Nougat, Green Tea"
+            tastingNotes: "Red Grapes, Peach, Nougat, Green Tea",
+            type: "coffee"
         },
         {
             id: 5,
             name: "Aurora",
             origin: "Nicaragua",
             price: "$11.00",
+            process: "Natural",
+            varietal: "Paraneima",
             description: "Natural, Paraneima",
-            tastingNotes: "Hazelnut, Honey, Lime, Rose Hips, Stone Fruit"
+            tastingNotes: "Hazelnut, Honey, Lime, Rose Hips, Stone Fruit",
+            type: "coffee"
         },
         {
             id: 6,
             name: "Nuna II",
             origin: "Colombia",
             price: "$14.00",
+            process: "Natural",
+            varietal: "Castillo",
             description: "Natural, Castillo",
-            tastingNotes: "Black Cherry, Guava, Sugar"
+            tastingNotes: "Black Cherry, Guava, Sugar",
+            type: "coffee"
         },
         {
             id: 7,
             name: "Orange Cream",
             origin: "Colombia",
             price: "$11.75",
+            process: "Washed",
+            varietal: "Caturra",
             description: "Washed, Caturra",
-            tastingNotes: "Orange, Marshmallow, Pear, Cane Sugar"
+            tastingNotes: "Orange, Marshmallow, Pear, Cane Sugar",
+            type: "coffee"
         },
         {
             id: 8,
             name: "Espresso",
-            origin: "Dak Monthly Subscription",
             price: "$26.00",
-            description: "",
-            tastingNotes: ""
+            description: "Dak Monthly Subscription",
+            type: "subscription"
         },
         {
             id: 9,
             name: "Filter",
-            origin: "Dak Monthly Subscription",
             price: "$26.00",
-            description: "",
-            tastingNotes: ""
+            description: "Dak Monthly Subscription",
+            type: "subscription"
         },
     ];
 
