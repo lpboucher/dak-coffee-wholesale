495aab2a8600217460295a9c961c8c338f9038e8
a-wilkes <a.sjwilkes@gmail.com>
2021-05-10 11:08:18 +0200

    feat(product page): add product page component

--- src/app/modules/products/products.common.ts
+++ src/app/modules/products/products.common.ts
@@ -2,8 +2,10 @@ import { Routes } from "@angular/router";
 
 import { ProductListComponent } from "@modules/products/components/product-list/product-list.component";
 import { ProductCardComponent } from '@modules/products/components/product-card/product-card.component';
+import { ProductPage } from './pages/product/product.page';
 
 export const pageDeclarations: any[] = [
+    ProductPage,
 ];
 
 export const componentDeclarations: any[] = [
