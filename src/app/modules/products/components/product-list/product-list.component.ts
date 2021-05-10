445551bc8382abd3c6991ff4b4bfec9c66304838
a-wilkes <a.sjwilkes@gmail.com>
2021-05-10 11:41:15 +0200

    feat(product list): inject product service

--- src/app/modules/products/components/product-list/product-list.component.ts
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '@app/core/products/product.service';
+
 @Component({
     selector: 'app-product-list',
     templateUrl: './product-list.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-    constructor() { }
+    constructor(productService: ProductService ) { }
 
     ngOnInit(): void {
     }
