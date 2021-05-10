cb4c7501bfcf73a32ccdee640637b003933306e1
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2021-05-10 18:16:43 +0200

    fix(product-list ts): use double quotes

--- src/app/modules/products/components/product-list/product-list.component.ts
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
 
-import { Coffee } from '@shared/models/classes/coffee.interface';
+import { Coffee } from "@shared/models/classes/coffee.interface";
 
-import { ProductService } from '@app/core/products/product.service';
+import { ProductService } from "@app/core/products/product.service";
 
 @Component({
-    selector: 'app-product-list',
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.scss']
+    selector: "app-product-list",
+    templateUrl: "./product-list.component.html",
+    styleUrls: ["./product-list.component.scss"]
 })
 export class ProductListComponent implements OnInit {
     products$: Observable<Coffee[]> = new Observable();
