99e5f7abfa9994fdd645acc3dd07ad881ba5c858
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2021-05-10 18:05:37 +0200

    fix(product page, common): use double quotes

--- src/app/modules/products/pages/product/product.page.ts
+++ src/app/modules/products/pages/product/product.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.page.html',
-  styleUrls: ['./product.page.scss']
+  selector: "app-product",
+  templateUrl: "./product.page.html",
+  styleUrls: ["./product.page.scss"]
 })
 export class ProductPageComponent implements OnInit {
 
--- src/app/modules/products/products.common.ts
+++ src/app/modules/products/products.common.ts
@@ -1,9 +1,9 @@
 import { Routes } from "@angular/router";
 
-import { ProductPageComponent } from '@modules/products/pages/product/product.page';
+import { ProductPageComponent } from "@modules/products/pages/product/product.page";
 
 import { ProductListComponent } from "@modules/products/components/product-list/product-list.component";
-import { ProductCardComponent } from '@modules/products/components/product-card/product-card.component';
+import { ProductCardComponent } from "@modules/products/components/product-card/product-card.component";
 
 export const pageDeclarations: any[] = [
     ProductPageComponent,
