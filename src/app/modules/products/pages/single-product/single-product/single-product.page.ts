b8ca8bf80c92f0326a9779757dd87f7aba432ac2
a-wilkes <a.sjwilkes@gmail.com>
2021-05-11 15:56:49 +0200

    feat(single product): get product from slug

--- src/app/modules/products/pages/single-product/single-product/single-product.page.ts
+++ src/app/modules/products/pages/single-product/single-product/single-product.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ProductService } from "@core/products/product.service";
+import { Observable } from "rxjs";
+
+import { Product } from "@shared/models/classes/product.class";
 
 @Component({
     selector: "app-single-product",
@@ -6,10 +11,22 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./single-product.page.scss"]
 })
 export class SingleProductPageComponent implements OnInit {
+    product?: Observable<Product | undefined>;
 
-    constructor() { }
+    constructor(
+        private route: ActivatedRoute,
+        private productService: ProductService,
+    ) { }
 
     ngOnInit(): void {
-    }
+        this.route.paramMap
+            .subscribe(params => {
+                const slug = params.get("slug");
+                if (slug === null) {
+                    return;
+                }
 
+                this.product = this.productService.getOne(slug);
+            });
+    }
 }
