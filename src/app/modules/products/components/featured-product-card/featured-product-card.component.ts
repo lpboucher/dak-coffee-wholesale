f67ecc94ca4795ebe03a9d116be443f3f87233d7
a-wilkes <a.sjwilkes@gmail.com>
2021-05-11 17:56:30 +0200

    feat(featured card): add base featured product card

---
+++ src/app/modules/products/components/featured-product-card/featured-product-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-featured-product-card',
+    templateUrl: './featured-product-card.component.html',
+    styleUrls: ['./featured-product-card.component.scss']
+})
+export class FeaturedProductCardComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
