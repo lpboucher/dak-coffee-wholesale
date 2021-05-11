842043385175253d7a7859643adca4940f17b6cf
a-wilkes <a.sjwilkes@gmail.com>
2021-05-11 16:03:05 +0200

    refactor(single product): fix folder structure and imports

--- src/app/modules/products/products.common.ts
+++ src/app/modules/products/products.common.ts
@@ -1,7 +1,7 @@
 import { Routes } from "@angular/router";
 
 import { ProductPageComponent } from "@modules/products/pages/product/product.page";
-import { SingleProductPageComponent } from './pages/single-product/single-product/single-product.page';
+import { SingleProductPageComponent } from "@modules/products/pages/single-product/single-product.page";
 
 import { ProductListComponent } from "@modules/products/components/product-list/product-list.component";
 import { ProductCardComponent } from "@modules/products/components/product-card/product-card.component";
