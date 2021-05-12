85d540cfbd8e0567e991a06153e645219392d5ec
a-wilkes <a.sjwilkes@gmail.com>
2021-05-12 09:54:11 +0200

    feat(product list): add base featured product cards to list view

--- src/app/modules/products/components/product-list/product-list.component.ts
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -11,11 +11,13 @@ import { Product } from "@shared/models/classes/product.class";
     styleUrls: ["./product-list.component.scss"]
 })
 export class ProductListComponent implements OnInit {
+    featuredProducts$: Observable<Product[]> = new Observable();
     products$: Observable<Product[]> = new Observable();
 
     constructor(private productService: ProductService) { }
 
     ngOnInit(): void {
+        this.featuredProducts$ = this.productService.getFeaturedProducts();
         this.products$ = this.productService.getProducts();
     }
 }
