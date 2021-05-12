5701b62dd78be82ec049ac5873de46521dd86107
a-wilkes <a.sjwilkes@gmail.com>
2021-05-12 17:40:47 +0200

    feat(login guard): ensure login guards checks for logged in status

--- src/app/core/guards/login.guard.ts
+++ src/app/core/guards/login.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "@core/authentication/authentication.service";
 
@@ -8,12 +8,19 @@ import { AuthService } from "@core/authentication/authentication.service";
 })
 export class LoginGuard implements CanActivate {
 
-    constructor(private authService: AuthService) {}
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+    ) {}
 
     canActivate(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        return true;
-    }
+        if (this.authService.isLoggedIn()) {
+            return true;
+        }
 
+        this.router.navigate(["auth"]);
+        return false;
+    }
 }
