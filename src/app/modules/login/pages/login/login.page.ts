80938ffb99b88ac9d066124dfd7996ee2cc040e0
a-wilkes <a.sjwilkes@gmail.com>
2021-05-12 16:17:22 +0200

    feat(login): add reactive login form

--- src/app/modules/login/pages/login/login.page.ts
+++ src/app/modules/login/pages/login/login.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: "app-login-page",
-  templateUrl: "./login.page.html",
-  styleUrls: ["./login.page.scss"]
+    selector: "app-login-page",
+    templateUrl: "./login.page.html",
+    styleUrls: ["./login.page.scss"],
 })
 export class LoginPageComponent implements OnInit {
+    loginForm = new FormGroup({
+        email: new FormControl(''),
+        password: new FormControl(''),
+    });
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    onSubmitLogin() {
+        console.log(this.loginForm.value);
+    }
 }
