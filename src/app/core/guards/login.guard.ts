2d74cabb4ed24f2aeaadc1a435ca194f1e7d2357
a-wilkes <a.sjwilkes@gmail.com>
2021-05-12 17:20:56 +0200

    refactor(login guard): inject AuthService and remove single quotes

--- src/app/core/guards/login.guard.ts
+++ src/app/core/guards/login.guard.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "@core/authentication/authentication.service";
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: "root"
 })
 export class LoginGuard implements CanActivate {
+
+    constructor(private authService: AuthService) {}
+
     canActivate(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
