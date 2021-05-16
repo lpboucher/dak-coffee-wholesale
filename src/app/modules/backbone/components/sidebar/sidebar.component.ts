9c79803a8d026a0ddda1b28fb81c16ea87c6dc8b
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2021-05-16 15:26:48 +0200

    change(sidebar ts html): add log out capability to sidebar

--- src/app/modules/backbone/components/sidebar/sidebar.component.ts
+++ src/app/modules/backbone/components/sidebar/sidebar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { AuthService } from "@core/authentication/authentication.service";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+    constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    onLogoutClicked(): void {
+        this.authService.logout();
+    }
 }
