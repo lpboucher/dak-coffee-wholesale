626c5179522eda8f2fff41e505851a4b363d4a08
a-wilkes <a.sjwilkes@gmail.com>
2021-05-17 13:42:49 +0200

    feat(login page): add form error messages

--- src/app/modules/login/pages/login/login.page.ts
+++ src/app/modules/login/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Validators, FormBuilder } from "@angular/forms";
+import { Validators, FormBuilder, AbstractControl } from "@angular/forms";
 import { AuthService } from "@core/authentication/authentication.service";
 
 @Component({
@@ -8,10 +8,14 @@ import { AuthService } from "@core/authentication/authentication.service";
     styleUrls: ["./login.page.scss"],
 })
 export class LoginPageComponent implements OnInit {
+    readonly minPasswordCharacters = 8;
+
     loginForm = this.fb.group({
         email: ["", [Validators.required, Validators.email]],
-        password: ["", Validators.required],
+        password: ["", [Validators.required, Validators.minLength(this.minPasswordCharacters)]],
     });
+
+    submissionAttempted = false;
 
     constructor(
         private fb: FormBuilder,
@@ -22,6 +26,8 @@ export class LoginPageComponent implements OnInit {
     }
 
     onSubmitLogin() {
+        this.submissionAttempted = true;
+
         const VALID_FORM_STATUS = "VALID";
         if (this.loginForm.status !== VALID_FORM_STATUS) {
             return;
@@ -29,5 +35,58 @@ export class LoginPageComponent implements OnInit {
 
         const input = this.loginForm.value;
         this.authService.login(input.email, input.password);
+    }
+
+    get emailControl(): AbstractControl {
+        return this.control("email")!;
+    }
+
+    get passwordControl(): AbstractControl {
+        return this.control("password")!;
+    }
+
+    shouldDisplayEmailError(): boolean {
+        return this.shouldDisplayError(this.emailControl);
+    }
+
+    shouldDisplayPasswordError(): boolean {
+        return this.shouldDisplayError(this.passwordControl);
+    }
+
+    shouldDisplayEmailMissing(): boolean {
+        return this.shouldDisplayMissing(this.emailControl);
+    }
+
+    shouldDisplayPasswordMissing(): boolean {
+        return this.shouldDisplayMissing(this.passwordControl);
+    }
+
+    shouldDisplayLoginFailed(): boolean {
+        return this.submissionAttempted
+            && this.loginForm.valid
+            && !this.authService.isLoggedIn();
+    }
+
+    formEntryTouched(): void {
+        this.submissionAttempted = false;
+    }
+
+    private control(name: string): AbstractControl | null {
+        return this.loginForm.get(name);
+    }
+
+    private shouldDisplayError(control: AbstractControl): boolean {
+        return this.submissionAttempted
+            && control.invalid
+            && !this.controlIsEmpty(control);
+    }
+
+    private shouldDisplayMissing(control: AbstractControl): boolean {
+        return this.submissionAttempted
+            && this.controlIsEmpty(control);
+    }
+
+    private controlIsEmpty(control: AbstractControl): boolean {
+        return control.pristine;
     }
 }
