e959be8222200f90cd17062d5cd13b3372cadf05
a-wilkes <a.sjwilkes@gmail.com>
2021-05-17 16:26:38 +0200

    refactor(merchandise): add material and dimensions to merchandise and create dummy merchandise object

---
+++ src/app/shared/models/classes/merchandise.class.ts
@@ -0,0 +1,22 @@
+import { ProductType } from "../types/product-type.type";
+import { Product } from "./product.class";
+
+export class Merchandise extends Product {
+    productType: ProductType = "merchandise";
+    dimensions: string | null = null;
+    material: string | null = null;
+
+    constructor(merchandiseShape?: Partial<Merchandise>) {
+        super(merchandiseShape);
+
+        if (merchandiseShape != null) {
+            if (merchandiseShape.dimensions != null) {
+                this.dimensions = merchandiseShape.dimensions;
+            }
+
+            if (merchandiseShape.material != null) {
+                this.material = merchandiseShape.material;
+            }
+        }
+    }
+}
