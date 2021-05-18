a1a2d255808b9c58845535675e6344772375354d
a-wilkes <a.sjwilkes@gmail.com>
2021-05-18 12:55:28 +0200

    feat(sidebar): use nav item component in sidebar

--- src/app/modules/backbone/components/sidebar/sidebar.component.ts
+++ src/app/modules/backbone/components/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
+import { NAVIGATION } from "@app/utils/constants/navigation";
+
 @Component({
     selector: "app-sidebar",
     templateUrl: "./sidebar.component.html",
     styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent implements OnInit {
+    navigation = NAVIGATION;
 
     constructor() { }
 
