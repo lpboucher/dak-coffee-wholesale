fd31d36c00f2f490187d816b36a0744a75a12baf
a-wilkes <a.sjwilkes@gmail.com>
2021-05-18 09:29:10 +0200

    feat(product page): display product types based on route

--- src/app/modules/products/components/product-list/product-list.component.ts
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { Observable } from "rxjs";
 
 import { ProductService } from "@core/products/product.service";
@@ -11,13 +11,12 @@ import { Product } from "@shared/models/classes/product.class";
     styleUrls: ["./product-list.component.scss"]
 })
 export class ProductListComponent implements OnInit {
+    @Input() products$: Observable<Product[]> = new Observable();
     featuredProducts$: Observable<Product[]> = new Observable();
-    products$: Observable<Product[]> = new Observable();
 
     constructor(private productService: ProductService) { }
 
     ngOnInit(): void {
         this.featuredProducts$ = this.productService.getFeaturedProducts();
-        this.products$ = this.productService.getProducts();
     }
 }
--- src/app/modules/products/pages/product/product.page.ts
+++ src/app/modules/products/pages/product/product.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { ProductService } from "@app/core/products/product.service";
+
+import { Product } from "@app/shared/models/classes/product.class";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: "app-product",
-  templateUrl: "./product.page.html",
-  styleUrls: ["./product.page.scss"]
+    selector: "app-product",
+    templateUrl: "./product.page.html",
+    styleUrls: ["./product.page.scss"]
 })
 export class ProductPageComponent implements OnInit {
+    products$: Observable<Product[]> = new Observable();
 
-  constructor() { }
+    constructor(
+        private productService: ProductService,
+        private route: ActivatedRoute,
+    ) { }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        const productsToShow = this.route.snapshot.data["productType"];
+        this.products$ = this.productService.getProductsByType(productsToShow);
+    }
 }
