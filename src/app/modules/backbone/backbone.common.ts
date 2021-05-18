8aa4bc183b7d9e0b92073c7140493cd9141cfed3
a-wilkes <a.sjwilkes@gmail.com>
2021-05-18 12:53:23 +0200

    feat(nav item): add nav item class to allow recursive sidebar items and interactivity

--- src/app/modules/backbone/backbone.common.ts
+++ src/app/modules/backbone/backbone.common.ts
@@ -3,6 +3,7 @@ import { Routes } from "@angular/router";
 import { BackbonePageComponent } from "@modules/backbone/pages/backbone/backbone.page";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { NavItemComponent } from "./components/nav-item/nav-item.component";
 
 export const pageDeclarations: any[] = [
     BackbonePageComponent,
@@ -10,7 +11,8 @@ export const pageDeclarations: any[] = [
 
 export const componentDeclarations: any[] = [
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    NavItemComponent,
 ];
 
 export const routes: Routes = [
