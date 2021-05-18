c6e49960067bd8609ae83fb6681a39d5075841b6
a-wilkes <a.sjwilkes@gmail.com>
2021-05-18 11:04:45 +0200

    feat(navigation): add navigation constants

---
+++ src/app/utils/constants/navigation.js
@@ -0,0 +1,17 @@
+export const NAVIGATION = [
+    {
+        label: "All Products",
+        link: "/products",
+        children: [],
+    },
+    {
+        label: "Coffees",
+        link: "/products/coffee",
+        children: [],
+    },
+    {
+        label: "Merchandise",
+        link: "/products/merchandise",
+        children: [],
+    },
+];
