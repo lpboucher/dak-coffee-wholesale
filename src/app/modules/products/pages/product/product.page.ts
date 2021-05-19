1e653671b0e7a53208507d2ae5efb7ebe95f1a8a
a-wilkes <a.sjwilkes@gmail.com>
2021-05-19 10:23:43 +0200

    fix(product page): correct imports

--- src/app/modules/products/pages/product/product.page.ts
+++ src/app/modules/products/pages/product/product.page.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 
-import { ProductService } from "@app/core/products/product.service";
+import { ProductService } from "@core/products/product.service";
 
-import { Product } from "@app/shared/models/classes/product.class";
+import { Product } from "@shared/models/classes/product.class";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
