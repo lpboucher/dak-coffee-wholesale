11d048df6ee534fb33105d4e74c19bce44da613a
a-wilkes <a.sjwilkes@gmail.com>
2021-05-19 16:46:33 +0200

    feat(filter): add base filter component

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -1,9 +1,11 @@
 import { GridListComponent } from "@shared/components/grid-list/grid-list.component";
 import { WalletWidgetComponent } from "@shared/components/wallet-widget/wallet-widget.component";
+import { FilterComponent } from "@shared/components/filter/filter.component";
 
 export const componentDeclarations: any[] = [
-        GridListComponent,
-        WalletWidgetComponent,
+    GridListComponent,
+    WalletWidgetComponent,
+    FilterComponent,
 ];
 
 export const providerDeclarations: any[] = [
