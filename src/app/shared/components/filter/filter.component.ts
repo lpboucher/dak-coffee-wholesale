050fd3eaebd1a8b26b3bc26aa5cd4a64e5c140d2
a-wilkes <a.sjwilkes@gmail.com>
2021-05-24 13:05:20 +0200

    feat(filter): add basic filter functionality and styles

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+
+type FilterType = { [key: string]: string[] };
 
 @Component({
     selector: "app-filter",
@@ -6,9 +9,59 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./filter.component.scss"]
 })
 export class FilterComponent implements OnInit {
+    private filterOptions: FilterType = {};
+    private activeFilters: FilterType = {};
 
-    constructor() { }
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute) { }
 
     ngOnInit(): void {
+        const options = {
+            "Origin": ["Colombia", "Rawanda", "Salvador"],
+            "Name": ["The Alchemist", "La Terraza", "La Dalia"],
+            "Price": ["$15.00", "$10.00"],
+        };
+
+        this.filterOptions = options;
+        console.log(this.filterOptions);
+    }
+
+    addToFilter(key: string, value: string): void {
+        if (this.activeFilters[key] == undefined) {
+            this.activeFilters[key] = [];
+        }
+
+        this.activeFilters[key].push(value);
+        this.updateQueryParams();
+    }
+
+    removeKeyFromFilter(key: string) {
+        delete this.activeFilters[key];
+        this.updateQueryParams();
+    }
+
+    removeOptionFromFilter(key: string, value: string): void {
+        this.activeFilters[key] = this.activeFilters[key]
+            .filter(f => f != value);
+
+        if (this.activeFilters[key].length == 0) {
+            this.removeKeyFromFilter(key);
+        } else {
+            this.updateQueryParams();
+        }
+    }
+
+    updateQueryParams(): void {
+        this.router.navigate([], { queryParams: this.activeFilters });
+        console.log(this.activeFilters);
+    }
+
+    availableFilterKeys(): string[] {
+        return Object.keys(this.filterOptions);
+    }
+
+    availableFilterKeyValues(key: string): string[] {
+        return this.filterOptions[key];
     }
 }
