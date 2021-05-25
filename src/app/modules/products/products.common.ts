04570c771b34e93d37845c82c99a20732eca0ce8
a-wilkes <a.sjwilkes@gmail.com>
2021-05-25 13:33:56 +0200

    refactor(product routes): change slug route to productType/slug

--- src/app/modules/products/products.common.ts
+++ src/app/modules/products/products.common.ts
@@ -37,7 +37,7 @@ export const routes: Routes = [
         children: []
     },
     {
-        path: ":slug",
+        path: ":productType/:slug",
         component: SingleProductPageComponent,
         children: []
     },
