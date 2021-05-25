f4eb36a6919d290c51de4c05d0781548ad9f2cf9
a-wilkes <a.sjwilkes@gmail.com>
2021-05-25 17:12:33 +0200

    refactor(product page): subsribe to changes in requested products

--- src/app/modules/products/pages/product/product.page.ts
+++ src/app/modules/products/pages/product/product.page.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 
 import { ProductService } from "@core/products/product.service";
 import { Product } from "@shared/models/classes/product.class";
+import { ProductType } from "@shared/models/types/product-type.type";
 
 @Component({
     selector: "app-product",
     templateUrl: "./product.page.html",
     styleUrls: ["./product.page.scss"]
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent implements OnInit, OnDestroy {
     featuredProducts$: Observable<Product[]> = new Observable;
     products$: Observable<Product[]> = new Observable();
+    paramMapSubscription$: Subscription;
 
     constructor(
         private productService: ProductService,
         private route: ActivatedRoute,
-    ) { }
+    ) {
+        this.paramMapSubscription$ = this.route.paramMap.subscribe(
+            paramMap => {
+                const productsToShow = paramMap.get("productType") as ProductType;
+                this.products$ = this.productService.getProductsByType(productsToShow);
+            }
+        );
+    }
 
     ngOnInit(): void {
         this.featuredProducts$ = this.productService.getFeaturedProducts();
+    }
 
-        const productsToShow = this.route.snapshot.data["productType"];
-        this.products$ = this.productService.getProductsByType(productsToShow);
+    ngOnDestroy(): void {
+        this.paramMapSubscription$.unsubscribe();
     }
 }
