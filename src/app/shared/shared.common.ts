bf85e36880a7358ce311e2381921fcea19530261
a-wilkes <a.sjwilkes@gmail.com>
2021-05-25 10:33:03 +0200

    feat(filter pipe): add base filter pipe

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -1,7 +1,9 @@
 import { FilterComponent } from "@shared/components/filter/filter.component";
+import { FilterPipe } from "@shared/pipes/filter.pipe";
 
 export const componentDeclarations: any[] = [
     FilterComponent,
+    FilterPipe,
 ];
 
 export const providerDeclarations: any[] = [
