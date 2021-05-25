3ff70f8969034cd13e62ef8c839f74ef138952b9
a-wilkes <a.sjwilkes@gmail.com>
2021-05-25 11:49:04 +0200

    feat(general info): add base general info page component

--- src/app/modules/general-info/general-info.common.ts
+++ src/app/modules/general-info/general-info.common.ts
@@ -1,6 +1,9 @@
 import { Routes } from "@angular/router";
 
+import { GeneralInfoPageComponent } from "./pages/general-info/general-info.page";
+
 export const pageDeclarations: any[] = [
+    GeneralInfoPageComponent,
 ];
 
 export const componentDeclarations: any[] = [
---
+++ src/app/modules/general-info/pages/general-info/general-info.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-general-info",
+    templateUrl: "./general-info.page.html",
+    styleUrls: ["./general-info.page.scss"]
+})
+export class GeneralInfoPageComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
