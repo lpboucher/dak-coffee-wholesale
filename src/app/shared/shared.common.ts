158d29bf65984d5932f77c798814d0716aa6bcdb
a-wilkes <a.sjwilkes@gmail.com>
2021-05-25 10:33:03 +0200

    feat(filter pipe): add base filter pipe

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -1,11 +1,13 @@
 import { GridListComponent } from "@shared/components/grid-list/grid-list.component";
 import { WalletWidgetComponent } from "@shared/components/wallet-widget/wallet-widget.component";
 import { FilterComponent } from "@shared/components/filter/filter.component";
+import { FilterPipe } from "@shared/pipes/filter.pipe";
 
 export const componentDeclarations: any[] = [
     GridListComponent,
     WalletWidgetComponent,
     FilterComponent,
+    FilterPipe,
 ];
 
 export const providerDeclarations: any[] = [
