6a8d5e70bfcb52591f2095d5890b0ff7a1327b58
a-wilkes <a.sjwilkes@gmail.com>
2021-05-25 11:15:40 +0200

    feat(filter pipe): add basic filtering for coffees

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 import { FilterType } from "@shared/models/types/filter-type.type";
 
 import { Product } from "@shared/models/classes/product.class";
+import { Coffee } from "@shared/models/classes/coffee.class";
 
 @Pipe({
     name: "filter"
@@ -10,6 +11,28 @@ import { Product } from "@shared/models/classes/product.class";
 export class FilterPipe implements PipeTransform {
 
     transform(products: Product[], filter: FilterType): Product[] {
-        return [];
+        let filteredProducts = [];
+
+        const coffees = products
+            .filter(p => p.productType == "coffee")
+            .map(p => p as Coffee);
+
+        for (const coffee of coffees) {
+            if (this.includedInFilter(coffee.origin, filter, "Origins")) {
+                filteredProducts.push(coffee);
+            } else if (this.includedInFilter(coffee.process, filter, "Processes")) {
+                filteredProducts.push(coffee);
+            } else if (this.includedInFilter(coffee.varietal, filter, "Varieties")) {
+                filteredProducts.push(coffee);
+            }
+        }
+
+        return filteredProducts;
+    }
+
+    includedInFilter(productAttribute: string | null, filter: FilterType, option: string): boolean {
+        return productAttribute != null
+            && Object.keys(filter).includes(option)
+            && filter[option].includes(productAttribute);
     }
 }
