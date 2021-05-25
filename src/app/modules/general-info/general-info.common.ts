cebf35755cd4d6470d2531facd437c8eea138a59
a-wilkes <a.sjwilkes@gmail.com>
2021-05-25 11:43:32 +0200

    feat(general info): add base general info module

---
+++ src/app/modules/general-info/general-info.common.ts
@@ -0,0 +1,13 @@
+import { Routes } from "@angular/router";
+
+export const pageDeclarations: any[] = [
+];
+
+export const componentDeclarations: any[] = [
+];
+
+export const providerDeclarations: any[] = [
+];
+
+export const routes: Routes = [
+];
