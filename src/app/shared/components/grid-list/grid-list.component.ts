aa536c4ef6025ef13be8e2570c9c44e960945315
a-wilkes <a.sjwilkes@gmail.com>
2021-05-26 13:25:39 +0200

    feat(grid list): implement grid list repsonsive columns

--- src/app/shared/components/grid-list/grid-list.component.ts
+++ src/app/shared/components/grid-list/grid-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: "app-grid-list",
@@ -6,9 +6,28 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./grid-list.component.scss"]
 })
 export class GridListComponent implements OnInit {
+    @Input() mobileColumnCount: number = 1;
 
     constructor() { }
 
     ngOnInit(): void {
+        this.clampColumnCount();
+    }
+
+    getColumnClass(): string {
+        return `mobile-columns-${ this.mobileColumnCount }`;
+    }
+
+    private clampColumnCount(): void {
+        const MAX_COLUMN_COUNT = 12;
+        const MIN_COLUMN_COUNT = 1;
+
+        if (this.mobileColumnCount > MAX_COLUMN_COUNT) {
+            this.mobileColumnCount = MAX_COLUMN_COUNT;
+        }
+
+        if (this.mobileColumnCount < MIN_COLUMN_COUNT) {
+            this.mobileColumnCount = MIN_COLUMN_COUNT;
+        }
     }
 }
