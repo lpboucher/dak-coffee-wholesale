b3591fb99e68676d0058c4a0097dcdf3032d0039
a-wilkes <a.sjwilkes@gmail.com>
2021-05-26 15:33:05 +0200

    feat(grid list): allow caller to select column width of children

--- src/app/shared/components/grid-list/grid-list.component.ts
+++ src/app/shared/components/grid-list/grid-list.component.ts
@@ -7,27 +7,30 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class GridListComponent implements OnInit {
     @Input() mobileColumnCount: number = 1;
+    @Input() itemColumnSpan: number = 1;
 
     constructor() { }
 
     ngOnInit(): void {
-        this.clampColumnCount();
+        this.mobileColumnCount = this.clampColumnValue(this.mobileColumnCount);
+        this.itemColumnSpan = this.clampColumnValue(this.itemColumnSpan);
     }
 
     getColumnClass(): string {
         return `mobile-columns-${ this.mobileColumnCount }`;
     }
 
-    private clampColumnCount(): void {
+    getItemColumnSpan(): string {
+        return `item-span-${ this.itemColumnSpan }`;
+    }
+
+    private clampColumnValue(columnValue: number): number {
         const MAX_COLUMN_COUNT = 12;
         const MIN_COLUMN_COUNT = 1;
 
-        if (this.mobileColumnCount > MAX_COLUMN_COUNT) {
-            this.mobileColumnCount = MAX_COLUMN_COUNT;
-        }
+        columnValue = Math.min(MAX_COLUMN_COUNT, columnValue);
+        columnValue = Math.max(MIN_COLUMN_COUNT, columnValue);
 
-        if (this.mobileColumnCount < MIN_COLUMN_COUNT) {
-            this.mobileColumnCount = MIN_COLUMN_COUNT;
-        }
+        return columnValue;
     }
 }
