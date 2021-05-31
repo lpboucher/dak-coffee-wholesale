876b6862621d4da6fab6a179afa5e564b5a05d0a
a-wilkes <a.sjwilkes@gmail.com>
2021-05-31 10:02:15 +0200

    feat(grid list): add grid gap option

--- src/app/shared/components/grid-list/grid-list.component.ts
+++ src/app/shared/components/grid-list/grid-list.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from "@angular/core";
 export class GridListComponent implements OnInit {
     @Input() mobileColumnCount: number = 1;
     @Input() itemColumnSpan: number = 1;
+    @Input() gridGap: number = 0;
 
     constructor() { }
 
@@ -22,6 +23,18 @@ export class GridListComponent implements OnInit {
 
     get itemColumnSpanClass(): string {
         return `item-span-${ this.itemColumnSpan }`;
+    }
+
+    get gridGapClass(): string {
+        const baseString = "grid-gap-";
+        const validGapValues = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 96];
+
+        if (validGapValues.includes(this.gridGap)) {
+            return baseString + this.gridGap.toString().replace(".", "-");
+        }
+
+        return baseString + "0";
     }
 
     private clampColumnValue(columnValue: number): number {
