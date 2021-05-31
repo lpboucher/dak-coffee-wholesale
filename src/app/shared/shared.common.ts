f91b78b855086b041ad1cffd528299f082a6619a
a-wilkes <a.sjwilkes@gmail.com>
2021-05-31 11:17:39 +0200

    feat(wallet): add base wallet component

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -1,5 +1,7 @@
+import { WalletWidgetComponent } from "@shared/components/wallet-widget/wallet-widget.component";
 
 export const componentDeclarations: any[] = [
+    WalletWidgetComponent,
 ];
 
 export const providerDeclarations: any[] = [
