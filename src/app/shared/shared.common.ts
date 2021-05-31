1747e7172a2933d6e02746746932e9f3113eb1c5
a-wilkes <a.sjwilkes@gmail.com>
2021-05-31 14:44:21 +0200

    feat(clamp): add base clamp pipe

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -1,10 +1,8 @@
 import { GridListComponent } from "@shared/components/grid-list/grid-list.component";
-import { WalletWidgetComponent } from "@shared/components/wallet-widget/wallet-widget.component";
 import { ClampPipe } from "@shared/pipes/clamp.pipe";
 
 export const componentDeclarations: any[] = [
     GridListComponent,
-    WalletWidgetComponent,
     ClampPipe,
 ];
 
