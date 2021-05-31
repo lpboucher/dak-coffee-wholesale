72f8e5d9d161f85d457af3dedae23a5add47e20b
a-wilkes <a.sjwilkes@gmail.com>
2021-05-31 12:51:39 +0200

    refactor(wallet): revert all wallet changes on develop branch

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -1,7 +1,5 @@
-import { WalletWidgetComponent } from "@shared/components/wallet-widget/wallet-widget.component";
 
 export const componentDeclarations: any[] = [
-    WalletWidgetComponent,
 ];
 
 export const providerDeclarations: any[] = [
