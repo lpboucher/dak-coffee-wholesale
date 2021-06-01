489c419c5c39cb14cac0f9733c21d03feb33d969
a-wilkes <a.sjwilkes@gmail.com>
2021-06-01 10:38:03 +0200

    refactor(filter): allow filtering on any type

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -2,37 +2,30 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 import { FilterType } from "@shared/models/types/filter-type.type";
 
-import { Product } from "@shared/models/classes/product.class";
-import { Coffee } from "@shared/models/classes/coffee.class";
-
 @Pipe({
     name: "filter"
 })
 export class FilterPipe implements PipeTransform {
 
-    transform(products: Product[], filter: FilterType): Product[] {
-        let filteredProducts = [];
+    transform(items: Object[], filter: FilterType): Object[] {
+        if (items == null) { return []; }
+        if (filter == null) { return items; }
 
-        const coffees = products
-            .filter(p => p.productType == "coffee")
-            .map(p => p as Coffee);
+        return items.filter(item => this.includedByFilter(filter, item));
+    }
 
-        for (const coffee of coffees) {
-            if (this.includedInFilter(coffee.origin, filter, "Origins")) {
-                filteredProducts.push(coffee);
-            } else if (this.includedInFilter(coffee.process, filter, "Processes")) {
-                filteredProducts.push(coffee);
-            } else if (this.includedInFilter(coffee.varietal, filter, "Varieties")) {
-                filteredProducts.push(coffee);
+    includedByFilter(filter: FilterType, item: Object): boolean {
+        for (const [key, value] of Object.keys(item)) {
+            if (this.matchingValue(filter, key, value)) {
+                return true;
             }
         }
 
-        return filteredProducts;
+        return false;
     }
 
-    includedInFilter(productAttribute: string | null, filter: FilterType, option: string): boolean {
-        return productAttribute != null
-            && Object.keys(filter).includes(option)
-            && filter[option].includes(productAttribute);
+    matchingValue(filter: FilterType, key: string, value: string): boolean {
+        return Object.keys(filter).includes(key)
+            && filter[key].includes(value);
     }
 }
