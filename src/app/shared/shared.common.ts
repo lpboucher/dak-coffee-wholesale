34a079f874aafcf9af5f540264ebd5b058c1fb74
a-wilkes <a.sjwilkes@gmail.com>
2021-06-02 13:44:21 +0200

    feat(modal): add base modal component

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -1,9 +1,11 @@
 import { GridListComponent } from "@shared/components/grid-list/grid-list.component";
 import { WalletWidgetComponent } from "@shared/components/wallet-widget/wallet-widget.component";
+import { ModalComponent } from "@shared/components/modal/modal.component";
 
 export const componentDeclarations: any[] = [
         GridListComponent,
         WalletWidgetComponent,
+        ModalComponent,
 ];
 
 export const providerDeclarations: any[] = [
