71ed266a94820e2e74fb1a7ba71888f3c30f7fbb
a-wilkes <a.sjwilkes@gmail.com>
2021-06-07 12:50:20 +0200

    feat(cart widget): add base cart widget component

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -2,6 +2,7 @@ import { FilterComponent } from "@shared/components/filter/filter.component";
 import { GridListComponent } from "@shared/components/grid-list/grid-list.component";
 import { WalletWidgetComponent } from "@shared/components/wallet-widget/wallet-widget.component";
 import { ModalComponent } from "@shared/components/modal/modal.component";
+import { CartWidgetComponent } from "@shared/components/cart-widget/cart-widget.component";
 
 import { ClampPipe } from "@shared/pipes/clamp.pipe";
 import { FilterPipe } from "@shared/pipes/filter.pipe";
@@ -11,6 +12,7 @@ export const componentDeclarations: any[] = [
     GridListComponent,
     ModalComponent,
     WalletWidgetComponent,
+    CartWidgetComponent,
 
     ClampPipe,
     FilterPipe,
