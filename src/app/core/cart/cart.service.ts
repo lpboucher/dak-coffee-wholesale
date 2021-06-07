e5668c48cc9c1aee3980af4f587a6bc70d7b204c
a-wilkes <a.sjwilkes@gmail.com>
2021-06-07 11:40:29 +0200

    feat(cart service): add functionality for cart total

--- src/app/core/cart/cart.service.ts
+++ src/app/core/cart/cart.service.ts
@@ -4,6 +4,45 @@ import { Injectable } from "@angular/core";
     providedIn: 'root'
 })
 export class CartService {
+    private snipcart: any = (window as any).Snipcart;
+    private _userToken?: string = undefined;
 
-    constructor() { }
+    constructor() {
+        // TODO: not waiting on the `document.snipcart.ready` event is a race condition
+        this.checkForOldCart();
+        this.watchForNewCarts();
+    }
+
+    get cartTotal(): number {
+        if (this._userToken == undefined) { return 0; }
+
+        return this.snipcart.store.getState().cart.total;
+    }
+
+    private checkForOldCart(): void {
+        this.snipcart.events.on("snipcart.initialized", (snipcartState: any) => {
+            if (snipcartState?.cart?.token == null) { return; }
+
+            console.log("Using cart from previous session.")
+            this.userToken = snipcartState.cart.token;
+        });
+    }
+
+    private watchForNewCarts(): void {
+        this.snipcart.events.on("cart.created", (cartState: any) => {
+            if (cartState?.token == null) { return; }
+
+            console.log("New cart created.");
+            this.userToken = cartState.token;
+        });
+    }
+
+    private set userToken(token: string) {
+        if (this._userToken != undefined) {
+            console.log(`Replacing old cart token: ${ this._userToken }`);
+        }
+
+        this._userToken = token;
+        console.log(`Cart token: ${ this._userToken }`);
+    }
 }
