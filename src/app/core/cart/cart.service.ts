c4259954cdde43480a0a5155bb8a7f6d8dbe72c1
a-wilkes <a.sjwilkes@gmail.com>
2021-06-07 14:11:45 +0200

    feat(cart service): add get cartWeight method

--- src/app/core/cart/cart.service.ts
+++ src/app/core/cart/cart.service.ts
@@ -11,4 +11,9 @@ export class CartService {
     get cartTotal(): number {
         return this.snipcart.store.getState().cart.total;
     }
+
+    get cartWeight(): number {
+        const cartItems = this.snipcart.store.getState().cart.items.items;
+        return cartItems.reduce((sum: number, item: any) => sum + (item.dimensions.weight * item.quantity), 0);
+    }
 }
