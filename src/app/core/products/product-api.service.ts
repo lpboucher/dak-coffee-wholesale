a097021fc1e3dfb973988313694230ddaab016b9
a-wilkes <a.sjwilkes@gmail.com>
2021-06-08 13:40:37 +0200

    refactor(coffee service): move coffee specific methods to separate service

--- src/app/core/products/product-api.service.ts
+++ src/app/core/products/product-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable, of } from "rxjs";
-import { take, map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 import { environment as config } from "@env";
 import { DataApiService } from "@core/abstracts/data-api.service";
@@ -9,96 +9,13 @@ import { Coffee } from "@shared/models/classes/coffee.class";
 import { Product } from "@shared/models/classes/product.class";
 import { ProductType } from "@shared/models/types/product-type.type";
 import { Merchandise } from "@shared/models/classes/merchandise.class";
+import { CoffeeApiService } from "./coffee-api.service";
 
 @Injectable({
     providedIn: "root"
 })
 export class ProductApiService extends DataApiService<Product> {
     products: Product[] = [
-        new Coffee({
-            id: "0",
-            name: "The Alchemist",
-            price: "15.00",
-            slug: "alchemist",
-            collection: "featured",
-            origin: "Rawanda",
-            tastingNotes: "Mango, Pineapple, Kiwi, Honey, Dark Chocolate",
-            process: "Experimental Natural",
-            varietal: "Bourbon",
-        }),
-        new Coffee({
-            id: "1",
-            name: "La Terraza",
-            price: "24.00",
-            slug: "terraza",
-            collection: "featured",
-            origin: "Colombia",
-            tastingNotes: "White Flower, Pomegranate, Honeycomb, Raspberry",
-            process: "Honey",
-            varietal: "Geisha",
-        }),
-        new Coffee({
-            id: "2",
-            name: "La Dalia",
-            price: "15.00",
-            slug: "dalia",
-            collection: "featured",
-            origin: "Salvador",
-            tastingNotes: "Green Apple, Red Currant, Strawberry",
-            process: "48hrs Anaerobic",
-            varietal: "Pacamara",
-        }),
-        new Coffee({
-            id: "3",
-            name: "El Diviso",
-            price: "16.00",
-            slug: "diviso",
-            collection: "upcoming",
-            origin: "Colombia",
-            tastingNotes: "Grapefruit, Orange, Peach, Sugar",
-            process: "Washed",
-            varietal: "Pink Bourbon",
-        }),
-        new Coffee({
-            id: "4",
-            name: "Rusatira",
-            price: "15.90",
-            slug: "rusatira",
-            origin: "Rwanda",
-            tastingNotes: "Red Grapes, Peach, Nougat, Green Tea",
-            process: "Wet Natural Anaerobic",
-            varietal: "Red Bourbon",
-        }),
-        new Coffee({
-            id: "5",
-            name: "Aurora",
-            price: "11.00",
-            slug: "aurora",
-            origin: "Nicaragua",
-            tastingNotes: "Hazelnut, Honey, Lime, Rose Hips, Stone Fruit",
-            process: "Natural",
-            varietal: "Paraneima",
-        }),
-        new Coffee({
-            id: "6",
-            name: "Nuna II",
-            price: "14.00",
-            slug: "nuna-ii",
-            origin: "Colombia",
-            tastingNotes: "Black Cherry, Guava, Sugar",
-            process: "Natural",
-            varietal: "Castillo",
-        }),
-        new Coffee({
-            id: "7",
-            name: "Orange Cream",
-            price: "11.75",
-            slug: "orange-cream",
-            origin: "Colombia",
-            process: "Washed",
-            varietal: "Caturra",
-            tastingNotes: "Orange, Marshmallow, Pear, Cane Sugar",
-        }),
         new Merchandise({
             id: "7",
             name: "Dak Tote Bag",
@@ -111,17 +28,22 @@ export class ProductApiService extends DataApiService<Product> {
         })
     ];
 
-    constructor(protected http: HttpClient) {
+    constructor(
+        protected http: HttpClient,
+        private coffeeApiService: CoffeeApiService,
+    ) {
         super(config.backendURL + "wholesale/", http, Coffee);
     }
 
     getProducts(): Observable<Product[]> {
-        return this.getAll("coffees?isActive=true");
-        // return of(this.products);
+        return (this.coffeeApiService.getCoffees() as Observable<Product[]>)
+            .pipe(
+                map(arr => { arr.push(...this.products); return arr; })
+            );
     }
 
     getProduct(slug: string): Observable<Product | undefined> {
-        return of(this.products)
+        return this.getProducts()
             .pipe(
                 map(arr => arr.find(p => p.slug === slug))
             );
@@ -133,7 +55,6 @@ export class ProductApiService extends DataApiService<Product> {
                 map(arr =>
                     arr.filter(p => p.collection === "featured")
                         .slice(0, 2)),
-                take(2)
             );
     }
 
