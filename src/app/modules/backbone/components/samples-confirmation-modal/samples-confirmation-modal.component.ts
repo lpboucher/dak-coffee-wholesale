fb9fe460e100328bc46d818b3fe3f2d2474fef2d
a-wilkes <a.sjwilkes@gmail.com>
2021-06-08 13:49:54 +0200

    feat(sample modal): add base sample confirmation modal

---
+++ src/app/modules/backbone/components/samples-confirmation-modal/samples-confirmation-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-samples-confirmation-modal",
+    templateUrl: "./samples-confirmation-modal.component.html",
+    styleUrls: ["./samples-confirmation-modal.component.scss"]
+})
+export class SamplesConfirmationModalComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
