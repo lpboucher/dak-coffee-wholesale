c982a160c4876a575c590f87b29b4247da8b24bd
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2021-06-08 19:22:38 +0200

    fix(cart service): getters above constructor

--- src/app/core/cart/cart.service.ts
+++ src/app/core/cart/cart.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@angular/core";
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: "root"
 })
 export class CartService {
     private snipcart: any = (window as any).Snipcart;
 
-    constructor() { }
-
     get cartTotal(): number {
         return this.snipcart.store.getState().cart.total;
     }
+
+    constructor() { }
 }
