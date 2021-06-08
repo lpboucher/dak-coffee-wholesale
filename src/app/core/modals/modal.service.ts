35e6679ec7105297538345ff0bf8cd74382a5d15
a-wilkes <a.sjwilkes@gmail.com>
2021-06-08 11:00:26 +0200

    feat(lazy modal): add open and close methods to modal service

--- src/app/core/modals/modal.service.ts
+++ src/app/core/modals/modal.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from "@angular/core";
+import { ApplicationRef, ComponentFactoryResolver, ComponentRef, EmbeddedViewRef, Injectable, Injector, Type } from "@angular/core";
 
 @Injectable({
     providedIn: "root"
 })
-export class ModalService {
+export class ModalService<T> {
+    private currentModelRef: ComponentRef<T> | undefined;
 
-    constructor() { }
+    constructor(
+        private componentFactoryResolver: ComponentFactoryResolver,
+        private applicationRef: ApplicationRef,
+        private injector: Injector,
+    ) { }
+
+    async open(component: Type<T>): Promise<void> {
+        if (this.currentModelRef) { return; }
+
+        this.currentModelRef = this.componentFactoryResolver
+            .resolveComponentFactory<T>(component)
+            .create(this.injector);
+
+        this.applicationRef.attachView(this.currentModelRef.hostView);
+
+        const domElem = (this.currentModelRef.hostView as EmbeddedViewRef<any>)
+            .rootNodes[0] as HTMLElement;
+
+        document.body.appendChild(domElem);
+    }
+
+    async close(): Promise<void> {
+        if (!this.currentModelRef) { return; }
+
+        this.applicationRef.detachView(this.currentModelRef.hostView);
+        this.currentModelRef.destroy();
+        this.currentModelRef = undefined;
+    }
 }
