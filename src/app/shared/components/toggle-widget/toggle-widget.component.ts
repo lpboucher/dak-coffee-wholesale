e36333f5a7134f1567ebf4acec2bec24ac0aaede
a-wilkes <a.sjwilkes@gmail.com>
2021-06-08 16:46:39 +0200

    feat(toggle): add base toggle widget

---
+++ src/app/shared/components/toggle-widget/toggle-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-toggle-widget",
+    templateUrl: "./toggle-widget.component.html",
+    styleUrls: ["./toggle-widget.component.scss"]
+})
+export class ToggleWidgetComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
