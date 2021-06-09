858638049337fe29d72fc6888c4a7aaa3fa2f21c
a-wilkes <a.sjwilkes@gmail.com>
2021-06-09 12:22:10 +0200

    feat(pricing service): add discount toggling behaviour

--- src/app/core/pricing/pricing-tier.service.ts
+++ src/app/core/pricing/pricing-tier.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root"
 })
 export class PricingTierService {
+    private isVolumeDiscountActive$ = new BehaviorSubject(false);
+
+    get isDiscountActive(): Observable<boolean> {
+        return this.isVolumeDiscountActive$.asObservable();
+    }
 
     constructor() { }
+
+    toggleDiscount(): void {
+        this.isVolumeDiscountActive$.next(!this.isVolumeDiscountActive$.value);
+    }
 }
