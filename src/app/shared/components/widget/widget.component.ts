b2a811ee705d157ebeee73b206941c19d69b5712
a-wilkes <a.sjwilkes@gmail.com>
2021-06-14 11:51:06 +0200

    feat(widget): add basic click handling

--- src/app/shared/components/widget/widget.component.ts
+++ src/app/shared/components/widget/widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: "app-widget",
@@ -7,10 +7,14 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class WidgetComponent implements OnInit {
     @Input() content: string = "";
+    @Output() clickEvent: EventEmitter<void> = new EventEmitter<void>();
 
     constructor() { }
 
     ngOnInit(): void {
     }
 
+    onClick(): void {
+        this.clickEvent.emit();
+    }
 }
