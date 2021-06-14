1113fe940a963b6a4a6193dbb9f061168cf84ebb
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2021-06-14 22:53:57 +0200

    add discount to constant file

--- src/app/shared/pipes/volume-discount.pipe.ts
+++ src/app/shared/pipes/volume-discount.pipe.ts
@@ -2,21 +2,21 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 import { PricingTierService } from "@core/pricing/pricing-tier.service";
 
+import { ACTIVE_DISCOUNT_REDUCTION, INACTIVE_DISCOUNT_REDUCTION} from "@utils/constants/discounts";
+
 @Pipe({
     name: "volumeDiscount"
 })
 export class VolumeDiscountPipe implements PipeTransform {
-    private static ACTIVE_DISCOUNT_REDUCTION: number = 0.55;
-    private static INACTIVE_DISCOUNT_REDUCTION: number = 0.7;
 
     constructor(private pricingTierService: PricingTierService) {}
 
     transform(value: number): number {
         if (this.pricingTierService.isDiscountActive) {
-            return value * VolumeDiscountPipe.ACTIVE_DISCOUNT_REDUCTION;
+            return value * ACTIVE_DISCOUNT_REDUCTION;
         }
 
-        return value * VolumeDiscountPipe.INACTIVE_DISCOUNT_REDUCTION;
+        return value * INACTIVE_DISCOUNT_REDUCTION;
     }
 
 }
