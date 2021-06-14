ba2a6508e28d903759fdead12bed62e91c354a26
a-wilkes <a.sjwilkes@gmail.com>
2021-06-14 16:31:26 +0200

    feat(snipcart directive): add functionality, and use in product cards

--- src/app/shared/directives/snipcart-add.directive.ts
+++ src/app/shared/directives/snipcart-add.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, Input, OnInit } from "@angular/core";
+
+import { Product } from "@shared/models/classes/product.class";
 
 @Directive({
     selector: "[appSnipcartAdd]"
 })
-export class SnipcartAddDirective {
+export class SnipcartAddDirective implements OnInit {
+    @Input() appSnipcartAdd!: Product;
 
-    constructor() { }
+    constructor(private el: ElementRef) {}
 
+    ngOnInit(): void {
+        this.el.nativeElement.className += " snipcart-add-item";
+
+        this.el.nativeElement.setAttribute("data-item-id", `${ this.appSnipcartAdd.id }`);
+        this.el.nativeElement.setAttribute("data-item-url", `/products/${ this.appSnipcartAdd.productType }/${ this.appSnipcartAdd.slug }`);
+        this.el.nativeElement.setAttribute("data-item-name", `${ this.appSnipcartAdd.name }`);
+        this.el.nativeElement.setAttribute("data-item-price", `${ this.appSnipcartAdd.price }`);
+    }
 }
