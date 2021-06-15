0bd182fb9d3a0a5c57103080a98ade5f7ef59991
a-wilkes <a.sjwilkes@gmail.com>
2021-06-15 15:52:08 +0200

    refactor(notify modal): rename confirmation modal to be more specific

---
+++ src/app/modules/products/components/get-notified-confirmation-modal/get-notified-confirmation-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-get-notified-confirmation-modal",
+    templateUrl: "./get-notified-confirmation-modal.component.html",
+    styleUrls: ["./get-notified-confirmation-modal.component.scss"]
+})
+export class GetNotifiedConfirmationModalComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
