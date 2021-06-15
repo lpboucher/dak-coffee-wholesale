f93a66deb5fdecabbe9fd691c42735b28748f7b3
a-wilkes <a.sjwilkes@gmail.com>
2021-06-15 16:00:03 +0200

    feat(notification modal): add view and style

--- src/app/modules/products/components/get-notified-confirmation-modal/get-notified-confirmation-modal.component.ts
+++ src/app/modules/products/components/get-notified-confirmation-modal/get-notified-confirmation-modal.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { ModalComponent } from "@shared/components/modal/modal.component";
 
 @Component({
     selector: "app-get-notified-confirmation-modal",
     templateUrl: "./get-notified-confirmation-modal.component.html",
     styleUrls: ["./get-notified-confirmation-modal.component.scss"]
 })
-export class GetNotifiedConfirmationModalComponent implements OnInit {
+export class GetNotifiedConfirmationModalComponent {
+    @ViewChild("modal") modal: ModalComponent | undefined;
 
-    constructor() { }
+    constructor() {}
 
-    ngOnInit(): void {
+    onCancel(): void {
+        this.close();
     }
 
+    onConfirm(): void {
+        this.close();
+    }
+
+    private close(): void {
+        if (this.modal != null) {
+            this.modal.close();
+        }
+    }
 }
