9acac8de3f8d253b7f2da6d2a4a6877ce585abc9
a-wilkes <a.sjwilkes@gmail.com>
2021-06-16 10:43:47 +0200

    refactor(grid list): allow selecting specific md column count

--- src/app/shared/components/grid-list/grid-list.component.ts
+++ src/app/shared/components/grid-list/grid-list.component.ts
@@ -3,6 +3,8 @@ import { ClampPipe } from "@shared/pipes/clamp.pipe";
 
 const MAX_COLUMN_COUNT = 12;
 const MIN_COLUMN_COUNT = 1;
+const MIN_GRID_GAP = 0;
+const MAX_GRID_GAP = 10;
 
 @Component({
     selector: "app-grid-list",
@@ -11,6 +13,7 @@ const MIN_COLUMN_COUNT = 1;
 })
 export class GridListComponent implements OnInit {
     @Input() mobileColumnCount: number = 1;
+    @Input() mdColumnCount?: number;
     @Input() itemColumnSpan: number = 1;
     @Input() gridGap: number = 0;
 
@@ -19,10 +22,23 @@ export class GridListComponent implements OnInit {
     ngOnInit(): void {
         this.mobileColumnCount = this.clampPipe.transform(this.mobileColumnCount, MIN_COLUMN_COUNT, MAX_COLUMN_COUNT);
         this.itemColumnSpan = this.clampPipe.transform(this.itemColumnSpan, MIN_COLUMN_COUNT, MAX_COLUMN_COUNT);
+        this.gridGap = this.clampPipe.transform(this.gridGap, MIN_GRID_GAP, MAX_GRID_GAP);
+
+        if (this.mdColumnCount != null) {
+            this.mdColumnCount = this.clampPipe.transform(this.mdColumnCount, MIN_COLUMN_COUNT, MAX_COLUMN_COUNT);
+        }
     }
 
     get columnClass(): string {
         return `mobile-columns-${ this.mobileColumnCount }`;
+    }
+
+    get mdColumnClass(): string {
+        if (this.mdColumnCount == null) {
+            return "";
+        }
+
+        return `md-columns-${ this.mdColumnCount }`;
     }
 
     get itemColumnSpanClass(): string {
