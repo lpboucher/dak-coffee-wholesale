dd2d1b482f339b758904b31ef5710cd0b8e9a49c
a-wilkes <a.sjwilkes@gmail.com>
2021-06-16 16:08:13 +0200

    feat(featured card): get images from cloudinary

--- src/app/modules/products/components/featured-product-card/featured-product-card.component.ts
+++ src/app/modules/products/components/featured-product-card/featured-product-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 
+import { Cloudinary } from "@cloudinary/base";
+
 import { Product } from "@shared/models/classes/product.class";
 
 @Component({
@@ -9,6 +11,16 @@ import { Product } from "@shared/models/classes/product.class";
 })
 export class FeaturedProductCardComponent implements OnInit {
     @Input() product!: Product;
+
+    get imageUrl(): string {
+        const cloud = new Cloudinary({
+            cloud: {
+                cloudName: "dak-coffee-roasters"
+            }
+        });
+
+        return cloud.image(`/Products/Thumbs/${ this.product.images.thumb }`).toURL();
+    }
 
     constructor() { }
 
