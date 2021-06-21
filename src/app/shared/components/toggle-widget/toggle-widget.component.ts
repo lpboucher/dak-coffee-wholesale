7da08ebb54a1185c4de988f94f7f634a744a2671
a-wilkes <a.sjwilkes@gmail.com>
2021-06-21 13:05:02 +0200

    refactor(toggle widget): use reactive forms

--- src/app/shared/components/toggle-widget/toggle-widget.component.ts
+++ src/app/shared/components/toggle-widget/toggle-widget.component.ts
@@ -1,22 +1,34 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
+import { FormBuilder, FormControl } from "@angular/forms";
 
 @Component({
     selector: "app-toggle-widget",
     templateUrl: "./toggle-widget.component.html",
     styleUrls: ["./toggle-widget.component.scss"]
 })
-export class ToggleWidgetComponent {
+export class ToggleWidgetComponent implements OnInit {
     @Input() header: string = "";
     @Input() uncheckedOption: string = "";
     @Input() checkedOption: string = "";
-    @Input() isChecked: boolean = false;
+    @Input() initialCheckboxState: boolean = false;
     @Output() toggle: EventEmitter<boolean> =  new EventEmitter();
 
+    toggleForm = this.fb.group({
+        toggle: new FormControl(),
+    });
 
-    constructor() { }
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.toggleForm.patchValue({
+            toggle: this.initialCheckboxState
+        });
+    }
 
     doToggle(): void {
-        this.isChecked = !this.isChecked;
-        this.toggle.emit(this.isChecked);
+        const toggleElement = this.toggleForm.get("toggle")!;
+        toggleElement.setValue(!toggleElement.value);
+
+        this.toggle.emit(toggleElement.value);
     }
 }
