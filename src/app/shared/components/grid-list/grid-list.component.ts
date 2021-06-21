20cf0c73a5f5624d6e512d01615bc6fee7cfe97f
a-wilkes <a.sjwilkes@gmail.com>
2021-06-21 16:04:06 +0200

    refactor(related products): remove mdColumnCount from grid list

--- src/app/shared/components/grid-list/grid-list.component.ts
+++ src/app/shared/components/grid-list/grid-list.component.ts
@@ -13,7 +13,6 @@ const MAX_GRID_GAP = 10;
 })
 export class GridListComponent implements OnInit {
     @Input() mobileColumnCount: number = 1;
-    @Input() mdColumnCount?: number;
     @Input() itemColumnSpan: number = 1;
     @Input() gridGap: number = 0;
 
@@ -23,22 +22,10 @@ export class GridListComponent implements OnInit {
         this.mobileColumnCount = this.clampPipe.transform(this.mobileColumnCount, MIN_COLUMN_COUNT, MAX_COLUMN_COUNT);
         this.itemColumnSpan = this.clampPipe.transform(this.itemColumnSpan, MIN_COLUMN_COUNT, MAX_COLUMN_COUNT);
         this.gridGap = this.clampPipe.transform(this.gridGap, MIN_GRID_GAP, MAX_GRID_GAP);
-
-        if (this.mdColumnCount != null) {
-            this.mdColumnCount = this.clampPipe.transform(this.mdColumnCount, MIN_COLUMN_COUNT, MAX_COLUMN_COUNT);
-        }
     }
 
     get columnClass(): string {
         return `mobile-columns-${ this.mobileColumnCount }`;
-    }
-
-    get mdColumnClass(): string {
-        if (this.mdColumnCount == null) {
-            return "";
-        }
-
-        return `md-columns-${ this.mdColumnCount }`;
     }
 
     get itemColumnSpanClass(): string {
