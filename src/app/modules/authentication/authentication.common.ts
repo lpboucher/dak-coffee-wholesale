dcc397c97656ed9d62a7371cc22c2a1cb1e7a929
a-wilkes <a.sjwilkes@gmail.com>
2021-06-22 11:10:13 +0200

    feat(request form): add base request access page

--- src/app/modules/authentication/authentication.common.ts
+++ src/app/modules/authentication/authentication.common.ts
@@ -1,11 +1,13 @@
 import { Routes } from "@angular/router";
 
 import { LoginPageComponent } from "@app/modules/authentication/pages/login/login.page";
+import { RequestAccessPage } from "@app/modules/authentication/pages/request-access/request-access.page";
 
 import { LoginComponent } from "@app/modules/authentication/components/login/login.component";
 
 export const pageDeclarations: any[] = [
     LoginPageComponent,
+    RequestAccessPage,
 ];
 
 export const componentDeclarations: any[] = [
