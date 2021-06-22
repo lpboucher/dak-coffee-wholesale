4fd1648963196134c9aa7add8f42f4bc2ca47b93
a-wilkes <a.sjwilkes@gmail.com>
2021-06-22 10:21:39 +0200

    refactor(modals): add index.ts file

---
+++ src/app/shared/components/modals/index.ts
@@ -0,0 +1,3 @@
+export { ModalBackboneComponent } from "./backbone/modal-backbone.component";
+export { SamplesConfirmationModalComponent } from "./content/samples-confirmation-modal/samples-confirmation-modal.component";
+export { WalletModalComponent } from "./content/wallet-modal/wallet-modal.component";
