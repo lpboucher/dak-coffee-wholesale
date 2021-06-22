f11e82e85b1f84f2671cb7d4026e50204691c8c8
a-wilkes <a.sjwilkes@gmail.com>
2021-06-22 11:12:47 +0200

    feat(request form): add base request access form component

--- src/app/modules/authentication/authentication.common.ts
+++ src/app/modules/authentication/authentication.common.ts
@@ -4,6 +4,7 @@ import { LoginPageComponent } from "@app/modules/authentication/pages/login/logi
 import { RequestAccessPage } from "@app/modules/authentication/pages/request-access/request-access.page";
 
 import { LoginComponent } from "@app/modules/authentication/components/login/login.component";
+import { RequestAccessFormComponent } from '@app/modules/authentication/components/request-access-form/request-access-form.component';
 
 export const pageDeclarations: any[] = [
     LoginPageComponent,
@@ -11,7 +12,8 @@ export const pageDeclarations: any[] = [
 ];
 
 export const componentDeclarations: any[] = [
-    LoginComponent
+    LoginComponent,
+    RequestAccessFormComponent,
 ];
 
 export const providerDeclarations: any[] = [
---
+++ src/app/modules/authentication/components/request-access-form/request-access-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-request-access-form",
+    templateUrl: "./request-access-form.component.html",
+    styleUrls: ["./request-access-form.component.scss"]
+})
+export class RequestAccessFormComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
