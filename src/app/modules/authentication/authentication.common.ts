a5de651c272550944dfac29aefd2a95474af29cd
a-wilkes <a.sjwilkes@gmail.com>
2021-06-22 17:21:53 +0200

    feat(access steps): add base request-access-steps comoponent

--- src/app/modules/authentication/authentication.common.ts
+++ src/app/modules/authentication/authentication.common.ts
@@ -5,6 +5,7 @@ import { RequestAccessPageComponent } from "@modules/authentication/pages/reques
 
 import { LoginComponent } from "@modules/authentication/components/login/login.component";
 import { RequestAccessFormComponent } from "@modules/authentication/components/request-access-form/request-access-form.component";
+import { RequestAccessStepsComponent } from "@modules/authentication/components/request-access-steps/request-access-steps.component";
 
 export const pageDeclarations: any[] = [
     LoginPageComponent,
@@ -14,6 +15,7 @@ export const pageDeclarations: any[] = [
 export const componentDeclarations: any[] = [
     LoginComponent,
     RequestAccessFormComponent,
+    RequestAccessStepsComponent,
 ];
 
 export const providerDeclarations: any[] = [
