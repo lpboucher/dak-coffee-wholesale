ad45cc9844d730fed2c14263746f077b2e2d5319
a-wilkes <a.sjwilkes@gmail.com>
2021-06-22 09:13:43 +0200

    feat(image service): add cloudinary image service

---
+++ src/app/core/views/image.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { CloudinaryImage } from "@cloudinary/base";
+
+const CLOUD_NAME = { cloudName: "dak-coffee-roasters" };
+
+@Injectable({
+    providedIn: "root"
+})
+export class ImageService {
+
+    constructor() { }
+
+    getImageUrl(path: string): string {
+        return new CloudinaryImage(path, CLOUD_NAME).toURL();
+    }
+
+    getProductThumbUrl(basename: string): string {
+        return this.getImageUrl(`/Products/Thumbs/${ basename }`);
+    }
+}
