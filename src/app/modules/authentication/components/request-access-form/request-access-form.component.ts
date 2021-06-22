9441edc275fe9fabb0cfa678f794316d07bacef9
a-wilkes <a.sjwilkes@gmail.com>
2021-06-22 14:10:05 +0200

    feat(register form): add basic view and functionality

--- src/app/modules/authentication/components/request-access-form/request-access-form.component.ts
+++ src/app/modules/authentication/components/request-access-form/request-access-form.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { AbstractControl, FormBuilder, Validators } from "@angular/forms";
+
+import { AuthService } from "@core/authentication/authentication.service";
 
 @Component({
     selector: "app-request-access-form",
     templateUrl: "./request-access-form.component.html",
     styleUrls: ["./request-access-form.component.scss"]
 })
-export class RequestAccessFormComponent implements OnInit {
+export class RequestAccessFormComponent {
 
-    constructor() { }
+    requestAccessForm = this.fb.group({
+        email: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required, Validators.minLength(8)]],
+        passwordConfirm: ["", Validators.required],
+        contactName: ["", Validators.required],
+        businessName: ["", Validators.required],
+        sector: ["", Validators.required],
+        vatNumber: [""],
+    });
 
-    ngOnInit(): void {
+    get contactNameControl(): AbstractControl {
+        return this.control("contactName")!;
     }
 
+    get businessNameControl(): AbstractControl {
+        return this.control("businessName")!;
+    }
+
+    get emailControl(): AbstractControl {
+        return this.control("email")!;
+    }
+
+    get passwordControl(): AbstractControl {
+        return this.control("password")!;
+    }
+
+    get passwordConfirmControl(): AbstractControl {
+        return this.control("passwordConfirm")!;
+    }
+
+    get sectorControl(): AbstractControl {
+        return this.control("sector")!;
+    }
+
+    get vatNumberControl(): AbstractControl {
+        return this.control("vatNumber")!;
+    }
+
+    constructor(
+        private fb: FormBuilder,
+        private authService: AuthService,
+    ) {}
+
+    onSubmitRegistration(): void {
+        if (this.requestAccessForm.invalid) { return; }
+
+        this.authService.register({
+            contactName: this.contactNameControl.value,
+            businessName: this.businessNameControl.value,
+            email: this.emailControl.value,
+            password: this.passwordControl.value,
+            sector: this.sectorControl.value,
+            vatNumber: this.vatNumberControl.value,
+        });
+    }
+
+    hasErrors(control: AbstractControl): boolean {
+        return control.invalid && (control.dirty || control.touched);
+    }
+
+    private control(name: string): AbstractControl | null {
+        return this.requestAccessForm.get(name);
+    }
 }
