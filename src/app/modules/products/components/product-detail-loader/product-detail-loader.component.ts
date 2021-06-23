944bfef27406402767b34288ae35573619c1e130
a-wilkes <a.sjwilkes@gmail.com>
2021-06-23 09:38:33 +0200

    refactor(product detail loader): move view and styles into own component

--- src/app/modules/products/components/product-detail-loader/product-detail-loader.component.ts
+++ src/app/modules/products/components/product-detail-loader/product-detail-loader.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-product-detail-loader",
     templateUrl: "./product-detail-loader.component.html",
     styleUrls: ["./product-detail-loader.component.scss"]
 })
-export class ProductDetailLoaderComponent implements OnInit {
-
-    constructor() { }
-
-    ngOnInit(): void {
-    }
-
+export class ProductDetailLoaderComponent {
+    constructor() {}
 }
