5dc62a511fa7161e9ef49b3f2edf2eae2531ac61
a-wilkes <a.sjwilkes@gmail.com>
2021-06-23 09:17:53 +0200

    feat(featured loader): add base featured product card loader component

---
+++ src/app/modules/products/components/featured-product-card-loader/featured-product-card-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-featured-product-card-loader",
+    templateUrl: "./featured-product-card-loader.component.html",
+    styleUrls: ["./featured-product-card-loader.component.scss"]
+})
+export class FeaturedProductCardLoaderComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
--- src/app/modules/products/products.common.ts
+++ src/app/modules/products/products.common.ts
@@ -6,6 +6,7 @@ import { SingleProductPageComponent } from "@modules/products/pages/single-produ
 import { ProductCardComponent } from "@modules/products/components/product-card/product-card.component";
 import { ProductDetailComponent } from "@modules/products/components/product-detail/product-detail.component";
 import { FeaturedProductCardComponent } from "@modules/products/components/featured-product-card/featured-product-card.component";
+import { FeaturedProductCardLoaderComponent } from "@modules/products/components/featured-product-card-loader/featured-product-card-loader.component";
 
 export const pageDeclarations: any[] = [
     ProductPageComponent,
@@ -16,6 +17,7 @@ export const componentDeclarations: any[] = [
     ProductCardComponent,
     ProductDetailComponent,
     FeaturedProductCardComponent,
+    FeaturedProductCardLoaderComponent,
 ];
 
 export const providerDeclarations: any[] = [
