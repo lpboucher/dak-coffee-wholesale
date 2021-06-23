66e6a15af405c0ea7d7eabb53778f60af61774bf
a-wilkes <a.sjwilkes@gmail.com>
2021-06-23 16:13:07 +0200

    refactor(pricing service): also toggle discount off on update

--- src/app/core/pricing/pricing-tier.service.ts
+++ src/app/core/pricing/pricing-tier.service.ts
@@ -24,8 +24,6 @@ export class PricingTierService {
     }
 
     updateDiscount(itemCount: number): void {
-        if (itemCount > ITEM_COUNT_THRESHOLD) {
-            this.toggleDiscount(true);
-        }
+        this.toggleDiscount(itemCount > ITEM_COUNT_THRESHOLD);
     }
 }
