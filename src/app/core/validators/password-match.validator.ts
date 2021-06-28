e6aee0f13ecf0940c634ca573cdca179c2d83f0f
a-wilkes <a.sjwilkes@gmail.com>
2021-06-28 16:01:59 +0200

    feat(password validator): move logic to password match validator

--- src/app/core/validators/password-match.validator.ts
+++ src/app/core/validators/password-match.validator.ts
@@ -2,6 +2,12 @@ import { ValidatorFn, ValidationErrors, AbstractControl } from "@angular/forms";
 
 export function PasswordMatch(passwordControlName: string, passwordMatchControlName: string): ValidatorFn {
     return (formGroup: AbstractControl): ValidationErrors | null => {
-        return null;
+        const passwordControlValue = formGroup.get(passwordControlName)?.value;
+        const passwordConfirmControlValue = formGroup.get(passwordMatchControlName)?.value;
+
+        if (passwordControlValue == null || passwordConfirmControlValue == null) { return null; }
+
+        const passwordsMatch = passwordControlValue === passwordConfirmControlValue;
+        return passwordsMatch ? null : { notMatching: true };
     };
 }
