c788492f29dc294296b3bf48276678ab956eea56
a-wilkes <a.sjwilkes@gmail.com>
2021-06-28 11:54:38 +0200

    feat(header): add base view and style

--- src/app/modules/backbone/components/header/header.component.ts
+++ src/app/modules/backbone/components/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+
+import { AuthService } from "@core/authentication/authentication.service";
+import { CartService } from "@core/cart/cart.service";
+import { PricingTierService } from "@core/pricing/pricing-tier.service";
+import { ModalService } from "@core/views/modal.service";
+
+import { WalletModalComponent } from "@shared/components/wallet-modal/wallet-modal.component";
+import { SidebarComponent } from "@modules/backbone/components/sidebar/sidebar.component";
 
 @Component({
     selector: "app-header",
     templateUrl: "./header.component.html",
     styleUrls: ["./header.component.scss"]
 })
-export class HeaderComponent implements OnInit {
-
-    constructor() { }
-
-    ngOnInit(): void {
+export class HeaderComponent extends SidebarComponent {
+    constructor(
+        authService: AuthService,
+        pricingTierService: PricingTierService,
+        cartService: CartService,
+        modalService: ModalService<WalletModalComponent>,
+    ) {
+        super(authService, pricingTierService, cartService, modalService);
     }
 
+    onBurgerMenuClick(): void {}
 }
