362e7a19ba5c98c4e888c28e5c9f38b7def2cefe
a-wilkes <a.sjwilkes@gmail.com>
2021-06-29 12:01:23 +0200

    feat(wallet widget): add logic

--- src/app/modules/backbone/components/wallet-widget/wallet-widget.component.ts
+++ src/app/modules/backbone/components/wallet-widget/wallet-widget.component.ts
@@ -1,12 +1,18 @@
 import { Component } from "@angular/core";
 
+import { ModalService } from "@core/views/modal.service";
+
+import { WalletModalComponent } from "@shared/components/wallet-modal/wallet-modal.component";
+
 @Component({
     selector: "app-wallet-widget",
     templateUrl: "./wallet-widget.component.html",
     styleUrls: ["./wallet-widget.component.scss"]
 })
 export class WalletWidgetComponent {
-    constructor() {}
+    constructor(private modalService: ModalService<WalletModalComponent>) {}
 
-    onWalletWidgetClick(): void {}
+    onWalletWidgetClick(): void {
+        this.modalService.open(WalletModalComponent);
+    }
 }
