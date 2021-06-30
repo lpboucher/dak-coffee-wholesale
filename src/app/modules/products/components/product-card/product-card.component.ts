c8c580ccca206438795131a8c48e71e010c214f6
a-wilkes <a.sjwilkes@gmail.com>
2021-06-30 16:33:42 +0200

    refactor(product card): replace dropdown with shared component

--- src/app/modules/products/components/product-card/product-card.component.ts
+++ src/app/modules/products/components/product-card/product-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from "@angular/core";
-import { AbstractControl, FormBuilder, Validators } from "@angular/forms";
 
 import { ImageService } from "@core/views/image.service";
 
@@ -16,64 +15,11 @@ export class ProductCardComponent {
     @Input() product!: Product;
     readonly weightOptions: Weight[] = ["250g", "1kg"];
     readonly roastOptions: Roast[] = ["Filter", "Espresso", "Both"];
-    selectionForm = this.fb.group({
-        weight: [this.weightOptions[0], Validators.required],
-        roast: [this.roastOptions[0], Validators.required]
-    });
-    showWeightOptions: boolean = false;
-    showRoastOptions: boolean = false;
-
-    get weightSelection(): Weight {
-        return this.weightControl.value;
-    }
-
-    set weightSelection(weight: Weight) {
-        this.weightControl.setValue(weight);
-    }
-
-    get weightControl(): AbstractControl {
-        return this.selectionForm.get("weight")!;
-    }
-
-    get roastSelection(): Roast {
-        return this.roastControl.value;
-    }
-
-    set roastSelection(roast: Roast) {
-        this.roastControl.setValue(roast);
-    }
-
-    get roastControl(): AbstractControl {
-        return this.selectionForm.get("roast")!;
-    }
 
     get imageUrl(): string {
         if (this.product.images.thumb == null) { return ""; }
         return this.imageService.getProductThumbUrl(this.product.images.thumb);
     }
 
-    constructor(
-        private imageService: ImageService,
-        private fb: FormBuilder,
-    ) {}
-
-    onClickWeightSelection(): void {
-        this.showWeightOptions = !this.showWeightOptions;
-    }
-
-    onSelectWeight(weight: Weight, event: Event): void {
-        this.weightSelection = weight;
-        this.showWeightOptions = false;
-        event.stopPropagation();
-    }
-
-    onClickRoastSelection(): void {
-        this.showRoastOptions = !this.showRoastOptions;
-    }
-
-    onSelectRoast(roast: Roast, event: Event): void {
-        this.roastSelection = roast;
-        this.showRoastOptions = false;
-        event.stopPropagation();
-    }
+    constructor(private imageService: ImageService) {}
 }
