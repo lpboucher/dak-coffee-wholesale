d6256212d24bc617dbff984e35123745b1272b29
a-wilkes <a.sjwilkes@gmail.com>
2021-06-30 18:22:25 +0200

    feat(snipcart directive): enable directive to handle custom options

--- src/app/shared/directives/snipcart-add.directive.ts
+++ src/app/shared/directives/snipcart-add.directive.ts
@@ -1,12 +1,14 @@
-import { Directive, ElementRef, Input, OnInit } from "@angular/core";
+import { Directive, ElementRef, Input, OnChanges, OnInit } from "@angular/core";
 
 import { Product } from "@shared/models/classes/product.class";
+import { CustomOption } from "@shared/models/classes/custom-option.class";
 
 @Directive({
     selector: "[snipcartAdd]"
 })
-export class SnipcartAddDirective implements OnInit {
+export class SnipcartAddDirective implements OnInit, OnChanges {
     @Input() snipcartAdd!: Product;
+    @Input() customOptions: CustomOption[] = [];
 
     constructor(private el: ElementRef) {}
 
@@ -20,5 +22,25 @@ export class SnipcartAddDirective implements OnInit {
         this.el.nativeElement.setAttribute("data-item-url", url);
         this.el.nativeElement.setAttribute("data-item-name", name);
         this.el.nativeElement.setAttribute("data-item-price", price);
+        this.setCustomAttributes();
+    }
+
+    ngOnChanges(): void {
+        this.setCustomAttributes();
+    }
+
+    private setCustomAttributes(): void {
+        this.customOptions.forEach((option, index) => {
+            const basename = `data-item-custom${ index }`;
+            const optionList = this.rawListToSnipcartList(option.list);
+
+            this.el.nativeElement.setAttribute(basename + "-name", option.name);
+            this.el.nativeElement.setAttribute(basename + "-options", optionList);
+            this.el.nativeElement.setAttribute(basename + "-value", option.selection);
+        });
+    }
+
+    private rawListToSnipcartList(raw: string[]): string {
+        return raw.reduce((prev, curr) => prev + "|" + curr);
     }
 }
