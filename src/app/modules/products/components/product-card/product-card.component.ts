1b84188fed36525812452b3ec4d9b52f112fd7ab
a-wilkes <a.sjwilkes@gmail.com>
2021-06-30 15:55:03 +0200

    feat(product card): add weight and roast dropdowns with full styling

--- src/app/modules/products/components/product-card/product-card.component.ts
+++ src/app/modules/products/components/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { FormBuilder, Validators } from "@angular/forms";
+import { AbstractControl, FormBuilder, Validators } from "@angular/forms";
 
 import { ImageService } from "@core/views/image.service";
 
@@ -20,6 +20,32 @@ export class ProductCardComponent {
         weight: [this.weightOptions[0], Validators.required],
         roast: [this.roastOptions[0], Validators.required]
     });
+    showWeightOptions: boolean = false;
+    showRoastOptions: boolean = false;
+
+    get weightSelection(): Weight {
+        return this.weightControl.value;
+    }
+
+    set weightSelection(weight: Weight) {
+        this.weightControl.setValue(weight);
+    }
+
+    get weightControl(): AbstractControl {
+        return this.selectionForm.get("weight")!;
+    }
+
+    get roastSelection(): Roast {
+        return this.roastControl.value;
+    }
+
+    set roastSelection(roast: Roast) {
+        this.roastControl.setValue(roast);
+    }
+
+    get roastControl(): AbstractControl {
+        return this.selectionForm.get("roast")!;
+    }
 
     get imageUrl(): string {
         if (this.product.images.thumb == null) { return ""; }
@@ -31,5 +57,23 @@ export class ProductCardComponent {
         private fb: FormBuilder,
     ) {}
 
-    onSelectionFormSubmit(): void {}
+    onClickWeightSelection(): void {
+        this.showWeightOptions = !this.showWeightOptions;
+    }
+
+    onSelectWeight(weight: Weight, event: Event): void {
+        this.weightSelection = weight;
+        this.showWeightOptions = false;
+        event.stopPropagation();
+    }
+
+    onClickRoastSelection(): void {
+        this.showRoastOptions = !this.showRoastOptions;
+    }
+
+    onSelectRoast(roast: Roast, event: Event): void {
+        this.roastSelection = roast;
+        this.showRoastOptions = false;
+        event.stopPropagation();
+    }
 }
