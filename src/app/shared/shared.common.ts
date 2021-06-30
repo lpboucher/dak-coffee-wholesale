85e62624d31a9360226677d29575c3db955efd9e
a-wilkes <a.sjwilkes@gmail.com>
2021-06-30 15:59:22 +0200

    feat(dropdown): add base dropdown component

--- src/app/shared/shared.common.ts
+++ src/app/shared/shared.common.ts
@@ -4,6 +4,7 @@ import { ModalComponent } from "@shared/components/modal/modal.component";
 import { ToggleWidgetComponent } from "@shared/components/toggle-widget/toggle-widget.component";
 import { WidgetComponent } from "@shared/components/widget/widget.component";
 import { WalletModalComponent } from '@shared/components/wallet-modal/wallet-modal.component';
+import { DropdownComponent } from "@shared/components/dropdown/dropdown.component";
 
 import { SnipcartAddDirective } from "@shared/directives/snipcart-add.directive";
 
@@ -19,6 +20,7 @@ export const componentDeclarations: any[] = [
     ToggleWidgetComponent,
     WidgetComponent,
     WalletModalComponent,
+    DropdownComponent,
 
     SnipcartAddDirective,
 
