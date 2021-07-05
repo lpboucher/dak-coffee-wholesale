05453db249a52d5433e1300de750fbd8794e49cc
a-wilkes <a.sjwilkes@gmail.com>
2021-07-05 18:31:57 +0200

    feat(quantity input): add style, view, and behaviour

--- src/app/shared/components/quantity-input/quantity-input.component.ts
+++ src/app/shared/components/quantity-input/quantity-input.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { AbstractControl, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     selector: "app-quantity-input",
@@ -6,5 +7,29 @@ import { Component } from "@angular/core";
     styleUrls: ["./quantity-input.component.scss"]
 })
 export class QuantityInputComponent {
-    constructor() {}
+    quantityForm = this.fb.group({
+        quantity: [1, [Validators.required, Validators.min(1)]],
+    });
+
+    constructor(private fb: FormBuilder) {}
+
+    onIncrement(): void {
+        const control = this.quantityControl();
+        this.verifyQuantity(control.value + 1);
+    }
+
+    onDecrement(): void {
+        const control = this.quantityControl();
+        this.verifyQuantity(control.value - 1);
+    }
+
+    verifyQuantity(value?: number): void {
+        const control = this.quantityControl();
+        const newValue = Math.max(1, value ?? control.value);
+        control.setValue(newValue);
+    }
+
+    private quantityControl(): AbstractControl {
+        return this.quantityForm.get("quantity")!;
+    }
 }
