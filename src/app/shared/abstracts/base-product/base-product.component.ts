3ea81f01b97f96e3f309850667eba9e1da3ed1ac
a-wilkes <a.sjwilkes@gmail.com>
2021-07-07 11:25:51 +0200

    feat(base product): add abstract base product

---
+++ src/app/shared/abstracts/base-product/base-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-base-product",
+    templateUrl: "",
+    styleUrls: [""]
+})
+export abstract class BaseProductComponent {
+    constructor() {}
+}
