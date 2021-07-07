8d7f09dccbbdcdcbd7f31e5a1b6280e0fb31b703
a-wilkes <a.sjwilkes@gmail.com>
2021-07-07 10:30:38 +0200

    refactor(product card): use weight and roast types

--- src/app/modules/products/components/product-card/product-card.component.ts
+++ src/app/modules/products/components/product-card/product-card.component.ts
@@ -7,6 +7,8 @@ import { CustomOption } from "@app/shared/models/types/custom-option.interface";
 import { Roast } from "@shared/models/types/roast.type";
 import { Weight } from "@shared/models/types/weight.type";
 
+import { ROAST_OPTIONS, WEIGHT_OPTIONS } from "@utils/constants/form-options";
+
 @Component({
     selector: "app-product-card",
     templateUrl: "./product-card.component.html",
@@ -15,13 +17,11 @@ import { Weight } from "@shared/models/types/weight.type";
 export class ProductCardComponent {
     @Input() product!: Product;
 
-    readonly weightOptions: Weight[] = ["250g", "1kg"];
-    readonly defaultWeight: Weight = this.weightOptions[0];
-    currentWeight: Weight = this.defaultWeight;
+    readonly weightOptions = WEIGHT_OPTIONS;
+    currentWeight: Weight = this.weightOptions[0];
 
-    readonly roastOptions: Roast[] = ["Filter", "Espresso", "Both"];
-    readonly defaultRoast: Roast = this.roastOptions[0];
-    currentRoast: Roast = this.defaultRoast;
+    readonly roastOptions = ROAST_OPTIONS;
+    currentRoast: Roast = this.roastOptions[0];
 
     snipcartOptions: CustomOption[] = this.makeSnipcartOptions();
 
@@ -49,8 +49,16 @@ export class ProductCardComponent {
 
     private makeSnipcartOptions(): CustomOption[] {
         return [
-            {name: "Weight", list: this.weightOptions, selection: this.currentWeight},
-            {name: "Roast", list: this.roastOptions, selection: this.currentRoast},
+            {
+                name: "Weight",
+                list: this.weightOptions.map(w => w as string),
+                selection: this.currentWeight
+            },
+            {
+                name: "Roast",
+                list: this.roastOptions.map(r => r as string),
+                selection: this.currentRoast
+            },
         ];
     }
 }
