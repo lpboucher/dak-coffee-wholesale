0fcf4cca558102f5ebf7e38d23299cde97c33b7b
a-wilkes <a.sjwilkes@gmail.com>
2021-07-12 12:33:08 +0200

    refactor(cart service): evaulate item weights using weight pipe

--- src/app/core/cart/cart.service.ts
+++ src/app/core/cart/cart.service.ts
@@ -3,6 +3,8 @@ import { BehaviorSubject, Observable } from "rxjs";
 
 import { PricingTierService } from "@core/pricing/pricing-tier.service";
 
+import { WeightPipe } from "@shared/pipes/weight.pipe";
+
 @Injectable({
     providedIn: 'root'
 })
@@ -18,7 +20,10 @@ export class CartService {
         return this.cartWeight$.asObservable();
     }
 
-    constructor(private pricingTierService: PricingTierService) {}
+    constructor(
+        private pricingTierService: PricingTierService,
+        private weightPipe: WeightPipe
+    ) {}
 
     openCart(): void {
         (window as any).Snipcart.api.theme.cart.open();
@@ -61,8 +66,13 @@ export class CartService {
 
     private evaluateCartWeight(items: any): number {
         return items
-            .filter((item: any) => item?.dimensions?.weight != null && item?.quantity != null)
-            .reduce((sum: number, item: any) => sum + (item.dimensions.weight * item.quantity), 0);
+            .map((item: any) => item.quantity * this.getSnipcartItemWeight(item))
+            .reduce((sum: number, weight: number) => sum + weight, 0);
+    }
+
+    private getSnipcartItemWeight(item: any): number {
+        let weightField = item.customFields.find((field: any) => field.name == "Weight");
+        return weightField?.value != null ? this.weightPipe.transform(weightField.value) : 0;
     }
 
     private updatePricingService(): void {
