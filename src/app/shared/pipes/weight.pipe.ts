3db87810029a2c25a3e795fccfa4b21de6f4e40a
a-wilkes <a.sjwilkes@gmail.com>
2021-07-12 10:59:28 +0200

    feat(weight pipe): add logic

--- src/app/shared/pipes/weight.pipe.ts
+++ src/app/shared/pipes/weight.pipe.ts
@@ -1,10 +1,20 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
+import { Weight } from "@shared/models/types/weight.type";
+
 @Pipe({
     name: "weight"
 })
 export class WeightPipe implements PipeTransform {
-    transform(value: unknown, ...args: unknown[]): unknown {
-        return null;
+    transform(weight: Weight): number {
+        const isGrams = !weight.includes("kg");
+        const rawWeightValue = weight.split(/kg|g/)[0];
+
+        let weightValue = Number.parseInt(rawWeightValue);
+        if (isGrams) {
+            weightValue = weightValue / 1000;
+        }
+
+        return weightValue;
     }
 }
