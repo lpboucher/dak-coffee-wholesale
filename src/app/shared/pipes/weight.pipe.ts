fb9dadc5f7dbaf2f5d2279ff8a0660cab6616000
a-wilkes <a.sjwilkes@gmail.com>
2021-07-12 15:58:32 +0200

    refactor(weight pipe): refactor let statement

--- src/app/shared/pipes/weight.pipe.ts
+++ src/app/shared/pipes/weight.pipe.ts
@@ -7,14 +7,10 @@ import { Weight } from "@shared/models/types/weight.type";
 })
 export class WeightPipe implements PipeTransform {
     transform(weight: Weight): number {
-        const isGrams = !weight.includes("kg");
+        const isKilos = weight.includes("kg");
         const rawWeightValue = weight.split(/kg|g/)[0];
 
-        let weightValue = Number.parseInt(rawWeightValue);
-        if (isGrams) {
-            weightValue = weightValue / 1000;
-        }
-
-        return weightValue;
+        const weightValue = Number.parseInt(rawWeightValue);
+        return isKilos ? weightValue : weightValue / 1000;
     }
 }
