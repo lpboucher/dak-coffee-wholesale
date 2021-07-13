6ed690328e38b254b69d628e0e88faa38d86d1d9
a-wilkes <a.sjwilkes@gmail.com>
2021-07-13 13:31:11 +0200

    feat(volume selection modal): add view, style, and logic

--- src/app/shared/components/modals/content/volume-selection-modal/volume-selection-modal.component.ts
+++ src/app/shared/components/modals/content/volume-selection-modal/volume-selection-modal.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { PricingTierService } from "@app/core/pricing/pricing-tier.service";
+
+import { ModalBackboneComponent } from "@shared/components/modals";
 
 @Component({
     selector: "app-volume-selection-modal",
     templateUrl: "./volume-selection-modal.component.html",
     styleUrls: ["./volume-selection-modal.component.scss"]
 })
-export class VolumeSelectionModalComponent implements OnInit {
-    constructor() {}
-    ngOnInit(): void {}
+export class VolumeSelectionModalComponent {
+    @ViewChild("modal") modal: ModalBackboneComponent | undefined;
+    largeVolumeDiscountSelected: boolean = false;
+
+    constructor(private pricingTierService: PricingTierService) {}
+
+    onChangeSelection(largeVolumeDiscountSelected: boolean): void {
+        this.largeVolumeDiscountSelected = largeVolumeDiscountSelected;
+    }
+
+    onConfirm(): void {
+        this.pricingTierService.toggleDiscount(this.largeVolumeDiscountSelected);
+
+        if (this.modal != null) {
+            this.modal.close();
+        }
+    }
 }
