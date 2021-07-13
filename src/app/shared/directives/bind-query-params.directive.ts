849ff78635bef4cfe7beee4becdf69fe2b1c161c
a-wilkes <a.sjwilkes@gmail.com>
2021-07-13 11:58:15 +0200

    feat(bind query params): add subscription to query params

--- src/app/shared/directives/bind-query-params.directive.ts
+++ src/app/shared/directives/bind-query-params.directive.ts
@@ -1,8 +1,33 @@
-import { Directive } from "@angular/core";
+import { Directive, OnDestroy, OnInit } from "@angular/core";
+import { ControlContainer } from "@angular/forms";
+import { ActivatedRoute, Params } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Directive({
-    selector: "[appBindQueryParams]"
+    selector: "[bindQueryParams]"
 })
-export class BindQueryParamsDirective {
-    constructor() {}
+export class BindQueryParamsDirective implements OnInit, OnDestroy {
+    private subscriptions: Subscription = new Subscription();
+
+    constructor(
+        private controlContainer: ControlContainer,
+        private activatedRoute: ActivatedRoute,
+    ) {}
+
+    ngOnInit(): void {
+        this.subscriptions.add(
+            this.activatedRoute.queryParams.subscribe(
+                (params: Params) => {
+                    this.controlContainer.control?.patchValue(
+                        params,
+                        { emitEvent: false }
+                    );
+                }
+            )
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
+    }
 }
