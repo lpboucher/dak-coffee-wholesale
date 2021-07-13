12f112f75c6f628977201a140c2d70fd18cead67
a-wilkes <a.sjwilkes@gmail.com>
2021-07-13 14:42:29 +0200

    feat(backbone): open pricing selection modal on loading from login

--- src/app/modules/backbone/pages/backbone/backbone.page.ts
+++ src/app/modules/backbone/pages/backbone/backbone.page.ts
@@ -1,14 +1,42 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Subscription } from "rxjs";
+
+import { ModalService } from "@core/views/modal.service";
+
+import { VolumeSelectionModalComponent } from "@shared/components/modals";
 
 @Component({
     selector: "app-backbone",
     templateUrl: "./backbone.page.html",
     styleUrls: ["./backbone.page.scss"]
 })
-export class BackbonePageComponent {
+export class BackbonePageComponent implements OnDestroy {
     showSidebar: boolean = false;
+    private subscriptions: Subscription = new Subscription();
 
-    constructor() {}
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private modalService: ModalService<VolumeSelectionModalComponent>,
+    ) {
+        this.subscriptions.add(
+            this.activatedRoute.queryParams.subscribe(
+                (params: Params) => {
+                    if (params["checkPricing"] === "true") {
+                        this.modalService.open(VolumeSelectionModalComponent);
+
+                        const { checkPricing, ...newParams } = params;
+                        this.router.navigate([], { queryParams: newParams });
+                    }
+                }
+            )
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
+    }
 
     onShowSidebar(): void {
         this.showSidebar = true;
