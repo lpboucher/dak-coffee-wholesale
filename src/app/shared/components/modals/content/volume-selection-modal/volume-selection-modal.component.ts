ad3b1342336b8361fefbdbd24170247edc2afec9
a-wilkes <a.sjwilkes@gmail.com>
2021-07-13 12:54:14 +0200

    feat(volume selection modal): add base modal component

---
+++ src/app/shared/components/modals/content/volume-selection-modal/volume-selection-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-volume-selection-modal",
+    templateUrl: "./volume-selection-modal.component.html",
+    styleUrls: ["./volume-selection-modal.component.scss"]
+})
+export class VolumeSelectionModalComponent implements OnInit {
+    constructor() {}
+    ngOnInit(): void {}
+}
