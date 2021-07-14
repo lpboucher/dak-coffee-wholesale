37c2840be1c11c45a9f2d1ba36841217b9614844
a-wilkes <a.sjwilkes@gmail.com>
2021-07-14 16:49:08 +0200

    feat(product page): add basic filter component to page

--- src/app/modules/products/pages/product/product.page.ts
+++ src/app/modules/products/pages/product/product.page.ts
@@ -3,7 +3,12 @@ import { ActivatedRoute } from "@angular/router";
 import { Observable, Subscription } from "rxjs";
 
 import { ProductApiService } from "@core/products/product-api.service";
+
 import { Product } from "@shared/models/classes/product.class";
+import { ProductType } from "@shared/models/types/product-type.type";
+import { FilterType } from "@shared/models/types/filter-type.type";
+
+import { getFilterableProperties } from "@utils/factories/filters";
 
 @Component({
     selector: "app-product",
@@ -14,6 +19,7 @@ export class ProductPageComponent implements OnInit, OnDestroy {
     private subscriptions = new Subscription();
     featuredProducts$: Observable<Product[]> = new Observable();
     products$: Observable<Product[]> = new Observable();
+    productType: ProductType | undefined;
 
     constructor(
         private productService: ProductApiService,
@@ -23,9 +29,10 @@ export class ProductPageComponent implements OnInit, OnDestroy {
     ngOnInit(): void {
         this.featuredProducts$ = this.productService.getFeaturedProducts();
 
-        this.subscriptions.add(this.route.params
-            .subscribe(
+        this.subscriptions.add(
+            this.route.params.subscribe(
                 ({productType}) => {
+                    this.productType = productType;
                     this.products$ = this.productService.getProductsByType(productType);
                 }
             )
@@ -34,5 +41,10 @@ export class ProductPageComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         this.subscriptions.unsubscribe();
+    }
+
+    getFilterableProperties(products: Product[] | null): FilterType {
+        if (products == null || this.productType == null) return {};
+        return getFilterableProperties(products, this.productType);
     }
 }
