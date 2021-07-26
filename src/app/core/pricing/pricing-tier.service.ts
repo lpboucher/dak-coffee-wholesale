276622a9c53e9303d143072c1abbe589a204f4da
a-wilkes <a.sjwilkes@gmail.com>
2021-07-26 17:44:54 +0200

    feat(pricing service): add method to calculate current wallet value

--- src/app/core/pricing/pricing-tier.service.ts
+++ src/app/core/pricing/pricing-tier.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
 
 import { CART_WEIGHT_THRESHOLD } from "@utils/constants/discounts";
+import { PREVIOUS_ORDER_PERCENTAGE_FOR_WALLET } from "@app/utils/constants/wallet";
+
+const DUMMY_ORDER = { previousWalletBallance: 200, previousOrderTotal: 50 };
 
 @Injectable({
     providedIn: "root"
@@ -15,6 +18,11 @@ export class PricingTierService {
 
     get isDiscountActive(): boolean {
         return this.isVolumeDiscountActive$.value;
+    }
+
+    get walletAmount(): number {
+        return DUMMY_ORDER.previousWalletBallance
+            + (DUMMY_ORDER.previousOrderTotal * PREVIOUS_ORDER_PERCENTAGE_FOR_WALLET);
     }
 
     constructor() {}
