52547e9d6844a70d200d867ff1a7c879309a1743
a-wilkes <a.sjwilkes@gmail.com>
2021-07-28 16:57:45 +0200

    feat(slide open menu): add behaviour, view, and style

--- src/app/shared/components/slide-open-menu/slide-open-menu.component.ts
+++ src/app/shared/components/slide-open-menu/slide-open-menu.component.ts
@@ -1,8 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "app-slide-open-menu",
     templateUrl: "./slide-open-menu.component.html",
     styleUrls: ["./slide-open-menu.component.scss"]
 })
-export class SlideOpenMenuComponent {}
+export class SlideOpenMenuComponent {
+    @Input() title = "";
+    showContent: boolean = false;
+
+    onClickHeader(): void {
+        this.showContent = !this.showContent;
+    }
+}
