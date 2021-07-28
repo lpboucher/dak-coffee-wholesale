f23e9e3efc1bc09fc6e6990f7f245460de0de1d1
a-wilkes <a.sjwilkes@gmail.com>
2021-07-28 11:54:48 +0200

    feat(flavour icon): add style, view, and behaviour

--- src/app/shared/components/flavour-icon/flavour-icon.component.ts
+++ src/app/shared/components/flavour-icon/flavour-icon.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "app-flavour-icon",
     templateUrl: "./flavour-icon.component.html",
     styleUrls: ["./flavour-icon.component.scss"]
 })
-export class FlavourIconComponent {}
+export class FlavourIconComponent {
+    @Input() flavour: string = "";
+
+    get svgSource(): string {
+        return `assets/svgs/flavour-icons/${ this.filename }.svg`;
+    }
+
+    private get filename(): string {
+        return this.flavour
+            .toLowerCase()
+            .replace(" ", "-");
+    }
+}
