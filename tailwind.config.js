7f4a444e8b2dea79421776b32f74277890c3c4c5
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2022-01-03 16:28:06 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,34 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: {
-    content: [
-      './src/**/*.{html,ts}',
-    ]
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-        opacity: ['disabled'],
-        cursor: ['disabled'],
+    purge: {
+        content: [
+        './src/**/*.{html,ts}',
+        ]
     },
-  },
-  plugins: [
-    require('@tailwindcss/custom-forms'),
-  ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        /*colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            gray: colors.coolGray,
+            red: colors.red,
+            blue: colors.sky,
+            green: colors.green,
+            yellow: colors.amber,
+            indigo: colors.indigo,
+            white: colors.white,
+            black: colors.black
+        },*/
+        extend: {},
+    },
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+            cursor: ['disabled'],
+        },
+    },
+    plugins: [
+        require('@tailwindcss/custom-forms'),
+    ],
 }
