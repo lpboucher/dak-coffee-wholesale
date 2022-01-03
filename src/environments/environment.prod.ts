c880f82e55d9a86da7af9c1d7111f84a7fd2e870
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2022-01-03 16:54:23 +0100

    change(config): adjust config for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: true,
-  backendURL: "https://dakcoffeeroasters.com/api/",
-  cloudinaryCloudName: "dak-coffee-roasters",
+    production: true,
+    backendURL: "https://dakcoffeeroasters.com/api/",
+    cloudinaryCloudName: "dak-coffee-roasters",
+    businessEmailAddress: "info@dakcoffeeroasters",
+    newProductLapse: 30 * 24 * 60 * 60 * 1000,
 };
